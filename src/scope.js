48c28f1d9ffe3b62213ceb32258e7eabc9c82b54
Tero Parviainen <tero.parviainen@eficode.fi>
2014-01-06 13:32:13 +0200

    Destroying Scopes

--- src/scope.js
+++ src/scope.js
@@ -43,6 +43,7 @@ Scope.prototype.$new = function(isolated, parent) {
   parent.$$children.push(child);
   child.$$watchers = [];
   child.$$children = [];
+  child.$parent = parent;
   return child;
 };
 
@@ -249,4 +250,15 @@ Scope.prototype.$$flushApplyAsync = function() {
 
 Scope.prototype.$$postDigest = function(fn) {
   this.$$postDigestQueue.push(fn);
+};
+
+Scope.prototype.$destroy = function() {
+  if (this === this.$$root) {
+    return;
+  }
+  var siblings = this.$parent.$$children;
+  var indexOfThis = siblings.indexOf(this);
+  if (indexOfThis >= 0) {
+    siblings.splice(indexOfThis, 1);
+  }
 };
