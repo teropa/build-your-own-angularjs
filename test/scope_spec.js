62e37fa07ce0fb3c3ac8b61495643d9f5d1fa555
Tero Parviainen <tero.parviainen@eficode.fi>
2014-01-06 12:46:44 +0200

    NaNs

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -208,6 +208,24 @@ describe("Scope", function() {
       expect(scope.counter).toBe(1);
     });
 
+    it("correctly handles NaNs", function() {
+      scope.number = 0/0; // NaN
+      scope.counter = 0;
+    
+      scope.$watch(
+        function(scope) { return scope.number; },
+        function(newValue, oldValue, scope) {
+          scope.counter++;
+        }
+      );
+  
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+  
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+    });
+
   });
 
 });
