d0d388e463eb37eefa03a899840f8e1b3b796439
Tero Parviainen <tero.parviainen@eficode.fi>
2014-01-06 12:51:19 +0200

    $apply - Integrating External Code With The Digest Cycle

--- src/scope.js
+++ src/scope.js
@@ -63,3 +63,11 @@ Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq) {
 Scope.prototype.$eval = function(expr, locals) {
   return expr(this, locals);
 };
+
+Scope.prototype.$apply = function(expr) {
+  try {
+    return this.$eval(expr);
+  } finally {
+    this.$digest();
+  }
+};
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -245,6 +245,28 @@ describe("Scope", function() {
   
       expect(result).toBe(44);
     });
+
+    it("Executes $apply'ed function and starts the digest", function() {
+      scope.aValue = 'someValue';
+      scope.counter = 0;
+
+      scope.$watch(
+        function(scope) {
+          return scope.aValue;
+        },
+        function(newValue, oldValue, scope) {
+          scope.counter++;
+        }
+      );
+  
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+
+      scope.$apply(function(scope) {
+        scope.aValue = 'someOtherValue';
+      });
+      expect(scope.counter).toBe(2);
+    });
     
   });
 
