7e1c0151dc96cfbb37967396ca0264f4ffcc5ef2
Tero Parviainen <tero.parviainen@eficode.fi>
2014-01-06 12:11:18 +0200

    Define The Default Grunt Task

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,4 +27,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
 
+  grunt.registerTask('default', ['jshint', 'jasmine']);
 };
