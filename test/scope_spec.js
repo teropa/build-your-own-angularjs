1147c8c8e94eaae15730a6a9d81bdd489e15d25c
Tero Parviainen <tero.parviainen@eficode.fi>
2014-01-06 12:45:01 +0200

    Value-Based Dirty-Checking

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -166,7 +166,26 @@ describe("Scope", function() {
       scope.array[0] = 420;
       scope.$digest();
       expect(watchExecutions).toBe(301);
-
+    });
+    
+    it("compares based on value if enabled", function() {
+      scope.aValue = [1, 2, 3];
+      scope.counter = 0;
+      
+      scope.$watch(
+        function(scope) { return scope.aValue; },
+        function(newValue, oldValue, scope) {
+          scope.counter++;
+        },
+        true
+      );
+  
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+  
+      scope.aValue.push(4);
+      scope.$digest();
+      expect(scope.counter).toBe(2);
     });
 
     it("does not end digest so that new watches are not run", function() {
