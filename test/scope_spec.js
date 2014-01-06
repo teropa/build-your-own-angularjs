a58efc8dfff67714919a137f732ca5df2043c820
Tero Parviainen <tero.parviainen@eficode.fi>
2014-01-06 13:15:22 +0200

    Making A Child Scope

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -852,4 +852,92 @@ describe("Scope", function() {
 
   });
 
+  describe("inheritance", function() {
+
+    it("inherits the parent's properties", function() {
+      var parent = new Scope();
+      parent.aValue = [1, 2, 3];
+
+      var child = parent.$new();
+
+      expect(child.aValue).toEqual([1, 2, 3]);
+    });
+
+    it("does not cause a parent to inherit its properties", function() {
+      var parent = new Scope();
+
+      var child = parent.$new();
+      child.aValue = [1, 2, 3];
+
+      expect(parent.aValue).toBeUndefined();
+    });
+
+    it("inherits the parent's properties whenever they are defined", function() {
+      var parent = new Scope();
+      var child = parent.$new();
+
+      parent.aValue = [1, 2, 3];
+
+      expect(child.aValue).toEqual([1, 2, 3]);
+    });
+
+    it("can manipulate a parent scope's property", function() {
+      var parent = new Scope();
+      var child = parent.$new();
+
+      parent.aValue = [1, 2, 3];
+      child.aValue.push(4);
+
+      expect(child.aValue).toEqual([1, 2, 3, 4]);
+      expect(parent.aValue).toEqual([1, 2, 3, 4]);
+    });
+
+    it("can watch a property in the parent", function() {
+      var parent = new Scope();
+      var child = parent.$new();
+
+      parent.aValue = [1, 2, 3];
+      child.counter = 0;
+
+      child.$watch(
+        function(scope) { return scope.aValue; },
+        function(newValue, oldValue, scope) {
+          scope.counter++;
+        },
+        true
+      );
+
+      child.$digest();
+      expect(child.counter).toBe(1);
+
+      parent.aValue.push(4);
+      child.$digest();
+      expect(child.counter).toBe(2);
+    });
+
+    it("can be nested at any depth", function() {
+      var a = new Scope();
+      var aa = a.$new();
+      var aaa = aa.$new();
+      var aab = aa.$new();
+      var ab = a.$new();
+      var abb = ab.$new();
+
+      a.value = 1;
+
+      expect(aa.value).toBe(1);
+      expect(aaa.value).toBe(1);
+      expect(aab.value).toBe(1);
+      expect(ab.value).toBe(1);
+      expect(abb.value).toBe(1);
+
+      ab.anotherValue = 2;
+
+      expect(abb.anotherValue).toBe(2);
+      expect(aa.anotherValue).toBeUndefined();
+      expect(aaa.anotherValue).toBeUndefined();
+    });
+
+  });
+
 });
