ee07cf61aafd788354bde4c108e9ec75430790c3
Tero Parviainen <tero.parviainen@eficode.fi>
2014-01-06 12:10:42 +0200

    Include Lo-Dash And jQuery

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,6 +26,8 @@ module.exports = function(grunt) {
           launch_in_dev: ['PhantomJS'],
           before_tests: 'grunt jshint',
           serve_files: [
+            'node_modules/lodash/index.js',
+            'node_modules/jquery/dist/jquery.js',
             'node_modules/sinon/pkg/sinon.js',
             'src/**/*.js',
             'test/**/*.js'
@@ -33,7 +35,7 @@ module.exports = function(grunt) {
           watch_files: [
             'src/**/*.js',
             'test/**/*.js'
-          ],
+          ]
         }
       }
     },
@@ -47,6 +49,5 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-testem');
-  grunt.loadNpmTasks('grunt-contrib-watch');
 
 };
