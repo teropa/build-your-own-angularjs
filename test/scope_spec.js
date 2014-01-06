24802907f88c6571cd1ac6b9c9515d999009fc58
Tero Parviainen <tero.parviainen@eficode.fi>
2014-01-06 12:40:21 +0200

    Giving Up On An Unstable Digest

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -123,7 +123,27 @@ describe("Scope", function() {
       scope.$digest();
       expect(scope.initial).toBe('B.');
     });
-    
+
+    it("gives up on the watches after 10 iterations", function() {
+      scope.counterA = 0;
+      scope.counterB = 0;
+
+      scope.$watch(
+        function(scope) { return scope.counterA; },
+        function(newValue, oldValue, scope) {
+          scope.counterB++;
+        }
+      );
+      scope.$watch(
+        function(scope) { return scope.counterB; },
+        function(newValue, oldValue, scope) {
+          scope.counterA++;
+        }
+      );
+
+      expect(function() { scope.$digest(); }).toThrow();
+    });
+
   });
 
 });
