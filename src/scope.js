3b2373faff7d063af57d06f016395ce7655ca6b8
Tero Parviainen <tero.parviainen@eficode.fi>
2014-01-06 13:00:48 +0200

    Running Code After A Digest - $$postDigest

--- src/scope.js
+++ src/scope.js
@@ -10,6 +10,7 @@ function Scope() {
   this.$$asyncQueue = [];
   this.$$applyAsyncQueue = [];
   this.$$applyAsyncId = null;
+  this.$$postDigestQueue = [];
   this.$$phase = null;
 }
 
@@ -67,6 +68,10 @@ Scope.prototype.$digest = function() {
     }
   } while (dirty || this.$$asyncQueue.length);
   this.$clearPhase();
+
+  while (this.$$postDigestQueue.length) {
+    this.$$postDigestQueue.shift()();
+  }
 };
 
 Scope.prototype.$$digestOnce = function() {
@@ -146,6 +151,10 @@ Scope.prototype.$$flushApplyAsync = function() {
     this.$$applyAsyncQueue.shift()();
   }
   this.$$applyAsyncId = null;
+};
+
+Scope.prototype.$$postDigest = function(fn) {
+  this.$$postDigestQueue.push(fn);
 };
 
 module.exports = Scope;
