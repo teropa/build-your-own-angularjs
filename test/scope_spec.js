6f972ef06b639b336872458407cfd63fecf548fb
Tero Parviainen <tero.parviainen@eficode.fi>
2014-01-06 12:57:56 +0200

    Scope Phases

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -372,6 +372,31 @@ describe('Scope', function() {
       expect(scope.counter).toBe(0);
     });
 
+    it('has a $$phase field whose value is the current digest phase', function() {
+      scope.aValue = [1, 2, 3];
+      scope.phaseInWatchFunction = undefined;
+      scope.phaseInListenerFunction = undefined;
+      scope.phaseInApplyFunction = undefined;
+
+      scope.$watch(
+        function(scope) {
+          scope.phaseInWatchFunction = scope.$$phase;
+          return scope.aValue;
+        },
+        function(newValue, oldValue, scope) {
+          scope.phaseInListenerFunction = scope.$$phase;
+        }
+      );
+
+      scope.$apply(function(scope) {
+        scope.phaseInApplyFunction = scope.$$phase;
+      });
+
+      expect(scope.phaseInWatchFunction).toBe('$digest');
+      expect(scope.phaseInListenerFunction).toBe('$digest');
+      expect(scope.phaseInApplyFunction).toBe('$apply');
+    });
+
   });
 
   describe('$eval', function() {
@@ -518,6 +543,26 @@ describe('Scope', function() {
       );
 
       expect(function() { scope.$digest(); }).toThrow();
+    });
+
+    it('schedules a digest in $evalAsync', function(done) {
+      scope.aValue = 'abc';
+      scope.counter = 0;
+
+      scope.$watch(
+        function(scope) { return scope.aValue; },
+        function(newValue, oldValue, scope) {
+          scope.counter++;
+        }
+      );
+
+      scope.$evalAsync(function(scope) { });
+      expect(scope.counter).toBe(0);
+
+      setTimeout(function() {
+        expect(scope.counter).toBe(1);
+        done();
+      }, 50);
     });
 
   });
