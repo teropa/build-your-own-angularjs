275f578c4d31087796568672b16bb5ce6dcbe2fe
Tero Parviainen <tero.parviainen@eficode.fi>
2014-01-06 12:08:55 +0200

    Enable Unit Testing With Jasmine, Sinon, and Testem

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,18 +2,51 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     jshint: {
-      all: ['src/**/*.js'],
+      all: ['src/**/*.js', 'test/**/*.js'],
       options: {
         globals: {
           _: false,
-          $: false
+          $: false,
+          jasmine: false,
+          describe: false,
+          it: false,
+          expect: false,
+          beforeEach: false,
+          afterEach: false,
+          sinon: false
         },
         browser: true,
         devel: true
+      }
+    },
+    testem: {
+      unit: {
+        options: {
+          framework: 'jasmine2',
+          launch_in_dev: ['PhantomJS'],
+          before_tests: 'grunt jshint',
+          serve_files: [
+            'node_modules/sinon/pkg/sinon.js',
+            'src/**/*.js',
+            'test/**/*.js'
+          ],
+          watch_files: [
+            'src/**/*.js',
+            'test/**/*.js'
+          ],
+        }
+      }
+    },
+    watch: {
+      all: {
+        files: ['src/**/*.js', 'test/**/*.js'],
+        tasks: ['jshint']
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-testem');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
 };
