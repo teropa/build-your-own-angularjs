21052620ebbad35315540fca46161607652531a9
Tero Parviainen <tero.parviainen@eficode.fi>
2014-01-06 12:24:41 +0200

    Watching Object Properties: $watch And $digest

--- src/scope.js
+++ src/scope.js
@@ -1,3 +1,17 @@
 function Scope() {
-
+	this.$$watchers = [];
 }
+
+Scope.prototype.$watch = function(watchFn, listenerFn) {
+  var watcher = {
+    watchFn: watchFn,
+    listenerFn: listenerFn
+  };
+  this.$$watchers.push(watcher);
+};
+
+Scope.prototype.$digest = function() {
+  this.$$watchers.forEach(function(watcher) {
+    watcher.listenerFn();
+  });
+};
