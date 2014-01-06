d768ac81f56388aa63701d955b38a9dfa2c912c7
Tero Parviainen <tero.parviainen@eficode.fi>
2014-01-06 12:22:12 +0200

    Scope Objects

---
+++ test/scope_spec.js
@@ -0,0 +1,10 @@
+describe("Scope", function() {
+
+  it("can be constructed and used as an object", function() {
+    var scope = new Scope();
+    scope.aProperty = 1;
+    
+    expect(scope.aProperty).toBe(1);
+  });
+
+});
