97ee1325e64d6ca76bede7bcf7f8bf6c175fd061
Tero Parviainen <tero.parviainen@eficode.fi>
2014-01-06 12:06:07 +0200

    Enable Static Analysis With JSHint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,19 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({  
+  grunt.initConfig({
+    jshint: {
+      all: ['src/**/*.js'],
+      options: {
+        globals: {
+          _: false,
+          $: false
+        },
+        browser: true,
+        devel: true
+      }
+    }
   });
-  
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
 };
