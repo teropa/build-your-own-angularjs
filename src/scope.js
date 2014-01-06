32cdbf2287887613905dbe6b6c3a17b5be2d6bfa
Tero Parviainen <tero.parviainen@eficode.fi>
2014-01-06 12:22:12 +0200

    Scope Objects

---
+++ src/scope.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function Scope() {
+
+}
+
+module.exports = Scope;
---
+++ test/scope_spec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var Scope = require('../src/scope');
+
+describe('Scope', function() {
+
+  it('can be constructed and used as an object', function() {
+    var scope = new Scope();
+    scope.aProperty = 1;
+
+    expect(scope.aProperty).toBe(1);
+  });
+
+});
