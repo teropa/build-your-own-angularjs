938b8b08d528b986e433d52b7af536bb849e4790
Tero Parviainen <tero.parviainen@eficode.fi>
2014-01-06 12:24:41 +0200

    Watching Object Properties: $watch And $digest

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -11,4 +11,24 @@ describe("Scope", function() {
     expect(scope.aProperty).toBe(1);
   });
 
+  describe("digest", function() {
+
+    var scope;
+
+    beforeEach(function() {
+      scope = new Scope();
+    });
+
+    it("calls the listener function of a watch on first $digest", function() {
+      var watchFn    = function() { return 'wat'; };
+      var listenerFn = jasmine.createSpy();
+      scope.$watch(watchFn, listenerFn);
+
+      scope.$digest();
+
+      expect(listenerFn).toHaveBeenCalled();
+    });
+
+  });
+
 });
