d9c126dd132bf6003b33e058344a85f76471ad4e
Tero Parviainen <tero.parviainen@eficode.fi>
2014-01-06 12:31:01 +0200

    Getting Notified Of Digests

--- src/scope.js
+++ src/scope.js
@@ -10,21 +10,31 @@ function Scope() {
 Scope.prototype.$watch = function(watchFn, listenerFn) {
   var watcher = {
     watchFn: watchFn,
-    listenerFn: listenerFn,
+    listenerFn: listenerFn || function() { },
     last: initWatchVal
   };
   this.$$watchers.push(watcher);
 };
 
+
 Scope.prototype.$digest = function() {
+  var dirty;
+  do {
+    dirty = this.$$digestOnce();
+  } while (dirty);
+};
+
+Scope.prototype.$$digestOnce = function() {
   var self = this;
-  var newValue, oldValue;
+  var newValue, oldValue, dirty;
   _.forEach(this.$$watchers, function(watcher) {
     newValue = watcher.watchFn(self);
     oldValue = watcher.last;
     if (newValue !== oldValue) {
       watcher.last = newValue;
       watcher.listenerFn(newValue, (oldValue === initWatchVal ? newValue : oldValue), self);
+      dirty = true;
     }
   });
+  return dirty;
 };
