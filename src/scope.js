b2a0c958dc5a2c66da8a89c903fbcd398cb759db
Tero Parviainen <tero.parviainen@eficode.fi>
2014-01-18 21:40:22 +0200

    Detecting Non-Collection Changes

--- src/scope.js
+++ src/scope.js
@@ -114,11 +114,24 @@ Scope.prototype.$watchGroup = function(watchFns, listenerFn) {
 };
 
 Scope.prototype.$watchCollection = function(watchFn, listenerFn) {
+  var self = this;
+  var newValue;
+  var oldValue;
+  var changeCount = 0;
 
   var internalWatchFn = function(scope) {
+    newValue = watchFn(scope);
+
+    if (newValue !== oldValue) {
+      changeCount++;
+    }
+    oldValue = newValue;
+
+    return changeCount;
   };
 
   var internalListenerFn = function() {
+    listenerFn(newValue, oldValue, self);
   };
 
   return this.$watch(internalWatchFn, internalListenerFn);
