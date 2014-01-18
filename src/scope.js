bac900f1ad3ae5e0677353b6325f9cd6129d755e
Tero Parviainen <tero.parviainen@eficode.fi>
2014-01-18 21:44:39 +0200

    Detecting New Or Removed Items in Arrays

--- src/scope.js
+++ src/scope.js
@@ -128,6 +128,10 @@ Scope.prototype.$watchCollection = function(watchFn, listenerFn) {
           changeCount++;
           oldValue = [];
         }
+        if (newValue.length !== oldValue.length) {
+          changeCount++;
+          oldValue.length = newValue.length;
+        }
       } else {
 
       }
