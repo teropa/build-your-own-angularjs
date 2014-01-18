0e768fa91d73aa272934f8df338ae573801651ab
Tero Parviainen <tero.parviainen@eficode.fi>
2014-01-18 21:55:01 +0200

    Detecting New Objects

--- src/scope.js
+++ src/scope.js
@@ -139,7 +139,10 @@ Scope.prototype.$watchCollection = function(watchFn, listenerFn) {
           }
         });
       } else {
-
+        if (!_.isObject(oldValue) || _.isArrayLike(oldValue)) {
+          changeCount++;
+          oldValue = {};
+        }
       }
     } else {
       if (!self.$$areEqual(newValue, oldValue, false)) {
