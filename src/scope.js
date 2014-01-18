e88e6c6634c2698b028809b76f1d41e2cad3ba4f
Tero Parviainen <tero.parviainen@eficode.fi>
2014-01-18 21:59:46 +0200

    Preventing Unnecessary Object Iteration

--- src/scope.js
+++ src/scope.js
@@ -117,10 +117,11 @@ Scope.prototype.$watchCollection = function(watchFn, listenerFn) {
   var self = this;
   var newValue;
   var oldValue;
+  var oldLength;
   var changeCount = 0;
 
   var internalWatchFn = function(scope) {
-    var key;
+    var newLength, key;
 
     newValue = watchFn(scope);
     if (_.isObject(newValue)) {
@@ -144,20 +145,32 @@ Scope.prototype.$watchCollection = function(watchFn, listenerFn) {
         if (!_.isObject(oldValue) || _.isArrayLike(oldValue)) {
           changeCount++;
           oldValue = {};
+          oldLength = 0;
         }
+				newLength = 0;
 				_.forOwn(newValue, function(newVal, key) {
-					var bothNaN = _.isNaN(newVal) && _.isNaN(oldValue[key]);
-					if (!bothNaN && oldValue[key] !== newVal) {
+					newLength++;
+					if (oldValue.hasOwnProperty(key)) {
+						var bothNaN = _.isNaN(newVal) && _.isNaN(oldValue[key]);
+						if (!bothNaN && oldValue[key] !== newVal) {
+							changeCount++;
+							oldValue[key] = newVal;
+						}
+					} else {
 						changeCount++;
+						oldLength++;
 						oldValue[key] = newVal;
 					}
 				});
-				_.forOwn(oldValue, function(oldVal, key) {
-					if (!newValue.hasOwnProperty(key)) {
-						changeCount++;
-						delete oldValue[key];
-					}
-				});
+        if (oldLength > newLength) {
+          changeCount++;
+          _.forOwn(oldValue, function(oldVal, key) {
+            if (!newValue.hasOwnProperty(key)) {
+              oldLength--;
+              delete oldValue[key];
+            }
+          });
+        }
       }
     } else {
       if (!self.$$areEqual(newValue, oldValue, false)) {
