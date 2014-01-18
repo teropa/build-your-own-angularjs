4d930f2885eb7889c4d6dde252ecd9960dc45c67
Tero Parviainen <tero.parviainen@eficode.fi>
2014-01-18 21:36:59 +0200

    Setting Up The Infrastructure for Watching Collections

--- src/scope.js
+++ src/scope.js
@@ -113,6 +113,16 @@ Scope.prototype.$watchGroup = function(watchFns, listenerFn) {
 	};
 };
 
+Scope.prototype.$watchCollection = function(watchFn, listenerFn) {
+
+  var internalWatchFn = function(scope) {
+  };
+
+  var internalListenerFn = function() {
+  };
+
+  return this.$watch(internalWatchFn, internalListenerFn);
+};
 
 Scope.prototype.$digest = function() {
   var ttl = 10;
