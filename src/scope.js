4deba51714b37f43bbec5ee320a6726c55d152ee
Tero Parviainen <tero.parviainen@eficode.fi>
2014-02-01 17:05:50 +0200

    Broadcasting Scope Removal

--- src/scope.js
+++ src/scope.js
@@ -348,6 +348,7 @@ Scope.prototype.$destroy = function() {
   var siblings = this.$parent.$$children;
   var indexOfThis = siblings.indexOf(this);
   if (indexOfThis >= 0) {
+    this.$broadcast('$destroy');
     siblings.splice(indexOfThis, 1);
   }
 };
