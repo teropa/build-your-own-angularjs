0bc5231b5455fb63cd75b9dc20ef23e656226383
Tero Parviainen <tero.parviainen@eficode.fi>
2014-02-01 16:40:10 +0200

    Broadcasting Down The Scope Hierarchy

--- src/scope.js
+++ src/scope.js
@@ -300,7 +300,10 @@ Scope.prototype.$emit = function(eventName) {
 Scope.prototype.$broadcast = function(eventName) {
   var event = {name: eventName};
   var listenerArgs = [event].concat(_.rest(arguments));
-  this.$$fireEventOnScope(eventName, listenerArgs);
+  this.$$everyScope(function(scope) {
+    scope.$$fireEventOnScope(eventName, listenerArgs);
+    return true;
+  });
   return event;
 };
 
