d464a0b76fe6c76f16fe2219b9cf6617c46a4ddf
Tero Parviainen <tero.parviainen@eficode.fi>
2014-02-01 15:25:20 +0200

    Events: Test Setup

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -1328,4 +1328,19 @@ describe("Scope", function() {
     
   });
 
+  describe("Events", function() {
+    var parent;
+    var scope;
+    var child;
+    var isolatedChild;
+  
+    beforeEach(function() {
+      parent = new Scope();
+      scope = parent.$new();
+      child = scope.$new();
+      isolatedChild = scope.$new(true);
+    });
+    
+  });
+
 });
