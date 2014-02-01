2b8089f0f3be9fc73534072dfd5f089db5654936
Tero Parviainen <tero.parviainen@eficode.fi>
2014-02-01 16:50:31 +0200

    Including The Current And Target Scopes in The Event Object

--- src/scope.js
+++ src/scope.js
@@ -377,23 +377,27 @@ Scope.prototype.$on = function(eventName, listener) {
 };
 
 Scope.prototype.$emit = function(eventName) {
-  var event = {name: eventName};
+  var event = {name: eventName, targetScope: this};
   var listenerArgs = [event].concat(_.tail(arguments));
   var scope = this;
   do {
+    event.currentScope = scope;
     scope.$$fireEventOnScope(eventName, listenerArgs);
     scope = scope.$parent;
   } while (scope);
+  event.currentScope = null;
   return event;
 };
 
 Scope.prototype.$broadcast = function(eventName) {
-  var event = {name: eventName};
+  var event = {name: eventName, targetScope: this};
   var listenerArgs = [event].concat(_.tail(arguments));
   this.$$everyScope(function(scope) {
+    event.currentScope = scope;
     scope.$$fireEventOnScope(eventName, listenerArgs);
     return true;
   });
+  event.currentScope = null;
   return event;
 };
 
