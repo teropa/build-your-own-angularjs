875a0550e8e83ac12977cdaf6aa6c680c32acef9
Tero Parviainen <tero@teropa.info>
2014-03-11 19:20:26 +0200

    Parsing Floating Point Numbers

--- src/parse.js
+++ src/parse.js
@@ -14,7 +14,8 @@ Lexer.prototype.lex = function(text) {
 
   while (this.index < this.text.length) {
     this.ch = this.text.charAt(this.index);
-    if (this.isNumber(this.ch)) {
+    if (this.isNumber(this.ch) ||
+          (this.ch === '.' && this.isNumber(this.peek()))) {
       this.readNumber();
     } else {
       throw 'Unexpected next character: '+this.ch;
@@ -33,7 +34,7 @@ Lexer.prototype.readNumber = function() {
 
   while (this.index < this.text.length) {
     var ch = this.text.charAt(this.index);
-    if (this.isNumber(ch)) {
+    if (ch === '.' || this.isNumber(ch)) {
       number += ch;
     } else {
       break;
@@ -48,6 +49,13 @@ Lexer.prototype.readNumber = function() {
     json: true
   });
 };
+
+Lexer.prototype.peek = function() {
+  return this.index < this.text.length - 1 ?
+    this.text.charAt(this.index + 1) :
+    false;
+};
+
 
 
 function Parser(lexer) {
