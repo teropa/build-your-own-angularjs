fc8dd08eedf20618a94a9b3eda04c283e9d5ee82
Tero Parviainen <tero@teropa.info>
2014-03-11 19:46:40 +0200

    Parsing Strings

--- src/parse.js
+++ src/parse.js
@@ -2,6 +2,8 @@
 /* jshint globalstrict: true */
 'use strict';
 
+var ESCAPES = {'n':'\n', 'f':'\f', 'r':'\r', 't':'\t', 'v':'\v', '\'':'\'', '"':'"'};
+
 function Lexer() {
 
 }
@@ -17,6 +19,8 @@ Lexer.prototype.lex = function(text) {
     if (this.isNumber(this.ch) ||
           (this.ch === '.' && this.isNumber(this.peek()))) {
       this.readNumber();
+    } else if (this.ch === '\'' || this.ch === '"') {
+      this.readString(this.ch);
     } else {
       throw 'Unexpected next character: '+this.ch;
     }
@@ -65,6 +69,49 @@ Lexer.prototype.readNumber = function() {
     fn: _.constant(number),
     json: true
   });
+};
+
+Lexer.prototype.readString = function(quote) {
+  this.index++;
+  var rawString = quote;
+  var string = '';
+  var escape = false;
+  while (this.index < this.text.length) {
+    var ch = this.text.charAt(this.index);
+    rawString += ch;
+    if (escape) {
+      if (ch === 'u') {
+        var hex = this.text.substring(this.index + 1, this.index + 5);
+        if (!hex.match(/[\da-f]{4}/i)) {
+          throw 'Invalid unicode escape';
+        }
+        this.index += 4;
+        string = String.fromCharCode(parseInt(hex, 16));
+      } else {
+        var replacement = ESCAPES[ch];
+        if (replacement) {
+          string += replacement;
+        } else {
+          string += ch;
+        }
+      }
+      escape = false;
+    } else if (ch === quote) {
+      this.index++;
+      this.tokens.push({
+        text: rawString,
+        json: true,
+        fn: _.constant(string)
+      });
+      return;
+    } else if (ch === '\\') {
+      escape = true;
+    } else {
+      string += ch;
+    }
+    this.index++;
+  }
+  throw 'Unmatched quote';
 };
 
 Lexer.prototype.peek = function() {
