dfdd73ffbeb38d2f4d87bc1316ee8652a218ac45
Tero Parviainen <tero@teropa.info>
2014-03-11 19:07:49 +0200

    Parsing Integers

--- src/parse.js
+++ src/parse.js
@@ -7,10 +7,46 @@ function Lexer() {
 }
 
 Lexer.prototype.lex = function(text) {
+  this.text = text;
+  this.index = 0;
+  this.ch = undefined;
+  this.tokens = [];
 
+  while (this.index < this.text.length) {
+    this.ch = this.text.charAt(this.index);
+    if (this.isNumber(this.ch)) {
+      this.readNumber();
+    } else {
+      throw 'Unexpected next character: '+this.ch;
+    }
+  }
+
+  return this.tokens;
 };
 
+Lexer.prototype.isNumber = function(ch) {
+  return '0' <= ch && ch <= '9';
+};
 
+Lexer.prototype.readNumber = function() {
+  var number = '';
+
+  while (this.index < this.text.length) {
+    var ch = this.text.charAt(this.index);
+    if (this.isNumber(ch)) {
+      number += ch;
+    } else {
+      break;
+    }
+    this.index++;
+  }
+
+  number = 1 * number;
+  this.tokens.push({
+    text: number,
+    fn: _.constant(number)
+  });
+};
 
 
 function Parser(lexer) {
@@ -19,6 +55,7 @@ function Parser(lexer) {
 
 Parser.prototype.parse = function(text) {
   this.tokens = this.lexer.lex(text);
+  return _.first(this.tokens).fn;
 };
 
 
