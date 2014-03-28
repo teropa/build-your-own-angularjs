7558b28471cb5a0e62c7d6bc78c07f3a4e93c1c4
Tero Parviainen <tero@teropa.info>
2014-03-28 07:03:54 +0200

    Function Calls

--- src/parse.js
+++ src/parse.js
@@ -1,4 +1,3 @@
-
 /* jshint globalstrict: true */
 'use strict';
 
@@ -73,7 +72,7 @@ Lexer.prototype.lex = function(text) {
       this.readNumber();
     } else if (this.is('\'"')) {
       this.readString(this.ch);
-    } else if (this.is('[],{}:.')) {
+    } else if (this.is('[],{}:.()')) {
       this.tokens.push({
         text: this.ch,
         json: true
@@ -220,6 +219,9 @@ Lexer.prototype.peek = function() {
     false;
 };
 
+Lexer.prototype.is = function(chs) {
+  return chs.indexOf(this.ch) >= 0;
+};
 
 
 function Parser(lexer) {
@@ -247,14 +249,15 @@ Parser.prototype.primary = function() {
   }
 
   var next;
-  while ((next = this.expect('[', '.'))) {
+  while ((next = this.expect('[', '.', '('))) {
      if (next.text === '[') {
       primary = this.objectIndex(primary);
     } else if (next.text === '.') {
       primary = this.fieldAccess(primary);
+    } else if (next.text === '(') {
+      primary = this.functionCall(primary);
     }
   }
-
   return primary;
 };
 
@@ -322,22 +325,35 @@ Parser.prototype.fieldAccess = function(objFn) {
   };
 };
 
+Parser.prototype.functionCall = function(fnFn) {
+  var argFns = [];
+  if (!this.peek(')')) {
+    do {
+      argFns.push(this.primary());
+    } while (this.expect(','));
+  }
+  this.consume(')');
+  return function(scope, locals) {
+    var fn = fnFn(scope, locals);
+    var args = _.map(argFns, function(argFn) { return argFn(scope, locals); });
+    return fn.apply(null, args);
+  };
+};
 
-Parser.prototype.peek = function(e) {
+Parser.prototype.peek = function(e1, e2, e3, e4) {
   if (this.tokens.length > 0) {
     var text = this.tokens[0].text;
-    if (text === e || !e) {
+    if (text === e1 || text === e2 || text === e3 || text === e4 ||
+        (!e1 && !e2 && !e3 && !e4)) {
       return this.tokens[0];
     }
   }
 };
 
-Parser.prototype.expect = function(e1, e2) {
-  if (this.tokens.length > 0) {
-    var text = this.tokens[0].text;
-    if (text === e1 || text === e2 || (!e1 && !e2)) {
-      return this.tokens.shift();
-    }
+Parser.prototype.expect = function(e1, e2, e3, e4) {
+  var token = this.peek(e1, e2, e3, e4);
+  if (token) {
+    return this.tokens.shift();
   }
 };
 
--- test/parse_spec.js
+++ test/parse_spec.js
@@ -252,4 +252,34 @@ describe("parse", function() {
     expect(fn({aKey: {anotherKey: {aThirdKey: {aFourthKey: 42}}}})).toBe(42);
   });
 
+  it('parses a function call', function() {
+    var fn = parse('aFunction()');
+    expect(fn({aFunction: function() { return 42; }})).toBe(42);
+  });
+
+  it('parses a function call with a single number argument', function() {
+    var fn = parse('aFunction(42)');
+    expect(fn({aFunction: function(n) { return n; }})).toBe(42);
+  });
+
+  it('parses a function call with a single identifier argument', function() {
+    var fn = parse('aFunction(n)');
+    expect(fn({n: 42, aFunction: function(arg) { return arg; }})).toBe(42);
+  });
+
+  it('parses a function call with a single function call argument', function() {
+    var fn = parse('aFunction(argFn())');
+    expect(fn({argFn: _.constant(42), aFunction: function(arg) { return arg; }})).toBe(42);
+  });
+
+  it('parses a function call with a multiple arguments', function() {
+    var fn = parse('aFunction(37, n, argFn())');
+    expect(fn({
+      n: 3,
+      argFn: _.constant(2),
+      aFunction: function(a1, a2, a3) { return a1 + a2 + a3; }
+    })).toBe(42);
+  });
+
+
 });
