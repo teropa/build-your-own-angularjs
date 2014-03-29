bdca8b4ad6fdf8b0d4371dd768d86d8152af3d56
Tero Parviainen <tero@teropa.info>
2014-03-29 13:25:37 +0200

    Handing The Old Collection Value To Listeners

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -1567,6 +1567,80 @@ describe("Scope", function() {
       expect(scope.counter).toBe(2);
     });
 
+    it("gives the old non-collection value to listeners", function() {
+      scope.aValue = 42;
+      var oldValueGiven;
+
+      scope.$watchCollection(
+        function(scope) { return scope.aValue; },
+        function(newValue, oldValue, scope) {
+          oldValueGiven = oldValue;
+        }
+      );
+
+      scope.$digest();
+
+      scope.aValue = 43;
+      scope.$digest();
+
+      expect(oldValueGiven).toBe(42);
+    });
+
+    it("gives the old array value to listeners", function() {
+      scope.aValue = [1, 2, 3];
+      var oldValueGiven;
+
+      scope.$watchCollection(
+        function(scope) { return scope.aValue; },
+        function(newValue, oldValue, scope) {
+          oldValueGiven = oldValue;
+        }
+      );
+
+      scope.$digest();
+
+      scope.aValue.push(4);
+      scope.$digest();
+
+      expect(oldValueGiven).toEqual([1, 2, 3]);
+    });
+
+    it("gives the old object value to listeners", function() {
+      scope.aValue = {a: 1, b: 2};
+      var oldValueGiven;
+
+      scope.$watchCollection(
+        function(scope) { return scope.aValue; },
+        function(newValue, oldValue, scope) {
+          oldValueGiven = oldValue;
+        }
+      );
+
+      scope.$digest();
+
+      scope.aValue.c = 3;
+      scope.$digest();
+
+      expect(oldValueGiven).toEqual({a: 1, b: 2});
+    });
+
+    it("uses the new value as the old value on first digest", function() {
+      scope.aValue = {a: 1, b: 2};
+      var oldValueGiven;
+
+      scope.$watchCollection(
+        function(scope) { return scope.aValue; },
+        function(newValue, oldValue, scope) {
+          oldValueGiven = oldValue;
+        }
+      );
+
+      scope.$digest();
+
+      expect(oldValueGiven).toEqual({a: 1, b: 2});
+    });
+
+
   });
 
 });
