ae4403d844e8606b5c0a0e3429c44eb872d04697
Tero Parviainen <tero@teropa.info>
2014-05-18 10:05:44 +0300

    Getting A Registered Module

--- test/loader_spec.js
+++ test/loader_spec.js
@@ -55,6 +55,20 @@ describe("setupModuleLoader", function() {
       expect(myModule.requires).toEqual(['myOtherModule']);
     });
 
+    it('allows getting a module', function() {
+      var myModule = window.angular.module('myModule', []);
+      var gotModule = window.angular.module('myModule');
+
+      expect(gotModule).toBeDefined();
+      expect(gotModule).toBe(myModule);
+    });
+
+   it('throws when trying to get a nonexisting module', function() {
+     expect(function() {
+       window.angular.module('myModule');
+     }).toThrow();
+   });
+
   });
 
 });
