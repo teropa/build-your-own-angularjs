48cb9f3b06cdd07d1364a94d89cc9d2240a522f1
Tero Parviainen <tero@teropa.info>
2014-05-18 12:51:24 +0300

    Providing Locals to Injected Functions

--- test/injector_spec.js
+++ test/injector_spec.js
@@ -124,4 +124,16 @@ describe('injector', function() {
     expect(injector.invoke(obj.fn, obj)).toBe(3);
   });
 
+  it('overrides dependencies with locals when invoking', function() {
+    var module = angular.module('myModule', []);
+    module.constant('a', 1);
+    module.constant('b', 2);
+    var injector = createInjector(['myModule']);
+
+    var fn = function(one, two) { return one + two; };
+    fn.$inject = ['a', 'b'];
+
+    expect(injector.invoke(fn, undefined, {b: 3})).toBe(4);
+  });
+
 });
