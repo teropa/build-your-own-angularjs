be337d0432c533b4a0d044566413c28fa890efc8
Tero Parviainen <tero@teropa.info>
2014-05-18 09:50:42 +0300

    Registering A Module

--- src/loader.js
+++ src/loader.js
@@ -8,9 +8,17 @@ function setupModuleLoader(window) {
 
   var angular = ensure(window, 'angular', Object);
 
-  ensure(angular, 'module', function() {
-    return function() {
+  var createModule = function(name, requires) {
+    var moduleInstance = {
+      name: name,
+      requires: requires
+    };
+    return moduleInstance;
+  };
 
+  ensure(angular, 'module', function() {
+    return function(name, requires) {
+      return createModule(name, requires);
     };
   });
 
