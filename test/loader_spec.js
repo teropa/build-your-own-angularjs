4c478c08033d31a9b08911bb9b5550c730e4cbe6
Tero Parviainen <tero@teropa.info>
2014-05-18 09:20:06 +0300

    Initializing The Global Just Once

--- test/loader_spec.js
+++ test/loader_spec.js
@@ -4,9 +4,20 @@
 
 describe("setupModuleLoader", function() {
 
+  beforeEach(function() {
+    delete window.angular;
+  });
+
   it('exposes angular on the window', function() {
     setupModuleLoader(window);
     expect(window.angular).toBeDefined();
+  });
+
+  it('creates angular just once', function() {
+    setupModuleLoader(window);
+    var ng = window.angular;
+    setupModuleLoader(window);
+    expect(window.angular).toBe(ng);
   });
 
 });
