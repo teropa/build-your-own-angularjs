fa9ba5a65159a4ec67692b8748da734e08e85f22
Tero Parviainen <tero@teropa.info>
2014-05-18 11:54:11 +0300

    Requiring Other Modules

--- src/injector.js
+++ src/injector.js
@@ -4,6 +4,7 @@ var _ = require('lodash');
 
 function createInjector(modulesToLoad) {
   var cache = {};
+  var loadedModules = {};
 
   var $provide = {
     constant: function(key, value) {
@@ -14,13 +15,17 @@ function createInjector(modulesToLoad) {
     }
   };
 
-  _.forEach(modulesToLoad, function(moduleName) {
-    var module = window.angular.module(moduleName);
-    _.forEach(module._invokeQueue, function(invokeArgs) {
-      var method = invokeArgs[0];
-      var args = invokeArgs[1];
-      $provide[method].apply($provide, args);
-    });
+  _.forEach(modulesToLoad, function loadModule(moduleName) {
+    if (!loadedModules.hasOwnProperty(moduleName)) {
+      loadedModules[moduleName] = true;
+      var module = window.angular.module(moduleName);
+      _.forEach(module.requires, loadModule);
+      _.forEach(module._invokeQueue, function(invokeArgs) {
+        var method = invokeArgs[0];
+        var args = invokeArgs[1];
+        $provide[method].apply($provide, args);
+      });
+    }
   });
 
   return {
