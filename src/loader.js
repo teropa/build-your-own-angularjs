b4b201beb2099f6c6bc84afe39021db3523ef23b
Tero Parviainen <tero@teropa.info>
2014-05-18 10:05:44 +0300

    Getting A Registered Module

--- src/loader.js
+++ src/loader.js
@@ -8,17 +8,34 @@ function setupModuleLoader(window) {
 
   var angular = ensure(window, 'angular', Object);
 
-  var createModule = function(name, requires) {
+  var createModule = function(name, requires, modules) {
+    if (name === 'hasOwnProperty') {
+      throw 'hasOwnProperty is not a valid module name';
+    }
     var moduleInstance = {
       name: name,
       requires: requires
     };
+    modules[name] = moduleInstance;
     return moduleInstance;
   };
 
+  var getModule = function(name, modules) {
+    if (modules.hasOwnProperty(name)) {
+      return modules[name];
+    } else {
+      throw 'Module '+name+' is not available!';
+    }
+  };
+
   ensure(angular, 'module', function() {
+    var modules = {};
     return function(name, requires) {
-      return createModule(name, requires);
+      if (requires) {
+        return createModule(name, requires, modules);
+      } else {
+        return getModule(name, modules);
+      }
     };
   });
 
