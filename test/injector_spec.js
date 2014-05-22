d34e56d121f5a86bf47fce90c2bdd1618825fcc5
Tero Parviainen <tero@teropa.info>
2014-05-22 07:24:20 +0300

    Singleton Enforcement

--- test/injector_spec.js
+++ test/injector_spec.js
@@ -351,4 +351,13 @@ describe('injector', function() {
     expect(injector.get('b')).toBe(3);
   });
 
+  it('instantiates a dependency only once', function() {
+    var module = angular.module('myModule', []);
+    module.provider('a', {$get: function() { return {}; }});
+
+    var injector = createInjector(['myModule']);
+
+    expect(injector.get('a')).toBe(injector.get('a'));
+  });
+
 });
