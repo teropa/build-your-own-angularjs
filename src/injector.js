c13d4d204f976fdca873d38238b4eb6c48aebca6
Tero Parviainen <tero@teropa.info>
2014-05-24 08:59:49 +0300

    Provider Constructors

--- src/injector.js
+++ src/injector.js
@@ -23,6 +23,9 @@ function createInjector(modulesToLoad, strictDi) {
       instanceCache[key] = value;
     },
     provider: function(key, provider) {
+      if (_.isFunction(provider)) {
+        provider = instantiate(provider);
+      }
       providerCache[key + 'Provider'] = provider;
     }
   };
