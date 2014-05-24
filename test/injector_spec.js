7c68268a5b3d07af34581a501d0bbbe3fd738955
Tero Parviainen <tero@teropa.info>
2014-05-24 10:47:32 +0300

    Two Injectors: The Provider Injector and The Instance Injector

--- test/injector_spec.js
+++ test/injector_spec.js
@@ -414,4 +414,84 @@ describe('injector', function() {
     expect(injector.get('a')).toBe(3);
   });
 
+  it('injects another provider to a provider constructor function', function() {
+    var module = angular.module('myModule', []);
+
+    module.provider('a', function AProvider() {
+      var value = 1;
+      this.setValue = function(v) { value = v; };
+      this.$get = function() { return value; };
+    });
+    module.provider('b', function BProvider(aProvider) {
+      aProvider.setValue(2);
+      this.$get = function() { };
+    });
+
+
+    var injector = createInjector(['myModule']);
+
+    expect(injector.get('a')).toBe(2);
+  });
+
+  it('does not inject an instance to a provider constructor function', function() {
+    var module = angular.module('myModule', []);
+
+    module.provider('a', function AProvider() {
+      this.$get = function() { return 1; };
+    });
+
+    module.provider('b', function BProvider(a) {
+      this.$get = function() { return a; };
+    });
+
+    expect(function() {
+      createInjector(['myModule']);
+    }).toThrow();
+
+  });
+
+  it('does not inject a provider to a $get function', function() {
+    var module = angular.module('myModule', []);
+
+    module.provider('a', function AProvider() {
+      this.$get = function() { return 1; };
+    });
+    module.provider('b', function BProvider() {
+      this.$get = function(aProvider) { return aProvider.$get(); };
+    });
+
+    var injector = createInjector(['myModule']);
+
+    expect(function() {
+      injector.get('b');
+    }).toThrow();
+  });
+
+  it('does not inject a provider to invoke', function() {
+    var module = angular.module('myModule', []);
+
+    module.provider('a', function AProvider() {
+      this.$get = function() { return 1; };
+    });
+
+    var injector = createInjector(['myModule']);
+
+    expect(function() {
+      injector.invoke(function(aProvider) { });
+    }).toThrow();
+  });
+
+  it('does not give access to providers through get', function() {
+    var module = angular.module('myModule', []);
+
+    module.provider('a', function AProvider() {
+      this.$get = function() { return 1; };
+    });
+
+    var injector = createInjector(['myModule']);
+    expect(function() {
+      injector.get('aProvider');
+    }).toThrow();
+  });
+
 });
