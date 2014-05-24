6704a8190384c2114e743f8b2d1dc879a0be03c9
Tero Parviainen <tero@teropa.info>
2014-05-24 13:26:44 +0300

    Unshifting Constants in The Invoke Queue

--- src/loader.js
+++ src/loader.js
@@ -15,9 +15,9 @@ function setupModuleLoader(window) {
 
     var invokeQueue = [];
 
-    var invokeLater = function(method) {
+    var invokeLater = function(method, arrayMethod) {
       return function() {
-        invokeQueue.push([method, arguments]);
+        invokeQueue[arrayMethod || 'push']([method, arguments]);
         return moduleInstance;
       };
     };
@@ -25,7 +25,7 @@ function setupModuleLoader(window) {
     var moduleInstance = {
       name: name,
       requires: requires,
-      constant: invokeLater('constant'),
+      constant: invokeLater('constant', 'unshift'),
       provider: invokeLater('provider'),
       _invokeQueue: invokeQueue
     };
--- test/injector_spec.js
+++ test/injector_spec.js
@@ -494,4 +494,16 @@ describe('injector', function() {
     }).toThrow();
   });
 
+  it('registers constants first to make them available to providers', function() {
+    var module = angular.module('myModule', []);
+
+    module.provider('a', function AProvider(b) {
+      this.$get = function() { return b; };
+    });
+    module.constant('b', 42);
+
+    var injector = createInjector(['myModule']);
+    expect(injector.get('a')).toBe(42);    
+  });
+
 });
