a81cf6124a5788f2a0c319024096225951734435
Tero Parviainen <tero@teropa.info>
2014-05-27 08:04:43 +0300

    Injecting The $injectors

--- src/injector.js
+++ src/injector.js
@@ -9,11 +9,13 @@ var INSTANTIATING = { };
 
 function createInjector(modulesToLoad) {
   var providerCache = {};
-  var providerInjector = createInternalInjector(providerCache, function() {
+  var providerInjector = providerCache.$injector =
+      createInternalInjector(providerCache, function() {
     throw 'Unknown provider: '+path.join(' <- ');
   });
   var instanceCache = {};
-  var instanceInjector = createInternalInjector(instanceCache, function(name) {
+  var instanceInjector = instanceCache.$injector =
+      createInternalInjector(instanceCache, function(name) {
     var provider = providerInjector.get(name + 'Provider');
     return instanceInjector.invoke(provider.$get, provider);
   });
