fd4f963e704c76ecd00bf23b25880bb7efe7da67
Tero Parviainen <tero@teropa.info>
2014-05-27 08:58:20 +0300

    Injecting $provide

--- src/injector.js
+++ src/injector.js
@@ -24,7 +24,7 @@ function createInjector(modulesToLoad, strictDi) {
   var path = [];
   strictDi = (strictDi === true);
 
-  var $provide = {
+  providerCache.$provide = {
     constant: function(key, value) {
       if (key === 'hasOwnProperty') {
         throw 'hasOwnProperty is not a valid constant name!';
@@ -124,7 +124,7 @@ function createInjector(modulesToLoad, strictDi) {
       _.forEach(module._invokeQueue, function(invokeArgs) {
         var method = invokeArgs[0];
         var args = invokeArgs[1];
-        $provide[method].apply($provide, args);
+        providerCache.$provide[method].apply(providerCache.$provide, args);
       });
     }
   });
