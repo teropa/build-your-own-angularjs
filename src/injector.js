c4582554241d93d106435c77e41b22185d38dd95
Tero Parviainen <tero@teropa.info>
2014-06-10 07:26:53 +0300

    Function Modules

--- src/injector.js
+++ src/injector.js
@@ -1,5 +1,5 @@
 /* jshint globalstrict: true */
-/* global angular: false */
+/* global angular: false, HashMap: false */
 'use strict';
 
 var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
@@ -20,7 +20,7 @@ function createInjector(modulesToLoad, strictDi) {
     var provider = providerInjector.get(name + 'Provider');
     return instanceInjector.invoke(provider.$get, provider);
   });
-  var loadedModules = {};
+  var loadedModules = new HashMap();
   var path = [];
   strictDi = (strictDi === true);
 
@@ -126,17 +126,21 @@ function createInjector(modulesToLoad, strictDi) {
   }
 
   var runBlocks = [];
-  _.forEach(modulesToLoad, function loadModule(moduleName) {
-    if (!loadedModules.hasOwnProperty(moduleName)) {
-      loadedModules[moduleName] = true;
-      var module = angular.module(moduleName);
-      _.forEach(module.requires, loadModule);
-      runInvokeQueue(module._invokeQueue);
-      runInvokeQueue(module._configBlocks);
-      runBlocks = runBlocks.concat(module._runBlocks);
+  _.forEach(modulesToLoad, function loadModule(module) {
+    if (!loadedModules.get(module)) {
+      loadedModules.put(module, true);
+      if (_.isString(module)) {
+        module = angular.module(module);
+        _.forEach(module.requires, loadModule);
+        runInvokeQueue(module._invokeQueue);
+        runInvokeQueue(module._configBlocks);
+        runBlocks = runBlocks.concat(module._runBlocks);
+      } else if (_.isFunction(module) || _.isArray(module)) {
+        runBlocks.push(providerInjector.invoke(module));
+      }
     }
   });
-  _.forEach(runBlocks, function(runBlock) {
+  _.forEach(_.compact(runBlocks), function(runBlock) {
     instanceInjector.invoke(runBlock);
   });
 
