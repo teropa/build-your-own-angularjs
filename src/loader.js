ff771f6c26884bb5691843199123f116e7000de7
Tero Parviainen <tero@teropa.info>
2014-06-10 07:55:36 +0300

    Factories

--- src/loader.js
+++ src/loader.js
@@ -29,6 +29,7 @@ function setupModuleLoader(window) {
       requires: requires,
       constant: invokeLater('$provide', 'constant', 'unshift'),
       provider: invokeLater('$provide', 'provider'),
+      factory: invokeLater('$provide', 'factory'),
       config: invokeLater('$injector', 'invoke', 'push', configBlocks),
       run: function(fn) {
         moduleInstance._runBlocks.push(fn);
