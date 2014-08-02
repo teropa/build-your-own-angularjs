c7cb5d1c715a6555d83e546c55c1d81518107043
Tero Parviainen <tero@teropa.info>
2014-08-02 19:47:08 +0300

    Compiling The DOM with Element Directives

--- src/compile.js
+++ src/compile.js
@@ -1,6 +1,11 @@
 'use strict';
 
 var _ = require('lodash');
+var $ = require('jquery');
+
+function nodeName(element) {
+  return element.nodeName ? element.nodeName : element[0].nodeName;
+}
 
 function $CompileProvider($provide) {
 
@@ -26,9 +31,43 @@ function $CompileProvider($provide) {
     }
   };
 
-  this.$get = function() {
+  this.$get = ['$injector', function($injector) {
 
-  };
+    function compile($compileNodes) {
+      return compileNodes($compileNodes);
+    }
+
+    function compileNodes($compileNodes) {
+      _.forEach($compileNodes, function(node) {
+        var directives = collectDirectives(node);
+        applyDirectivesToNode(directives, node);
+      });
+    }
+
+    function collectDirectives(node) {
+      var directives = [];
+      var normalizedNodeName = _.camelCase(nodeName(node).toLowerCase());
+      addDirective(directives, normalizedNodeName);
+      return directives;
+    }
+
+    function addDirective(directives, name) {
+      if (hasDirectives.hasOwnProperty(name)) {
+        directives.push.apply(directives, $injector.get(name + 'Directive'));
+      }
+    }
+
+    function applyDirectivesToNode(directives, compileNode) {
+      var $compileNode = $(compileNode);
+      _.forEach(directives, function(directive) {
+        if (directive.compile) {
+          directive.compile($compileNode);
+        }
+      });
+    }
+
+    return compile;
+  }];
 
 }
 $CompileProvider.$inject = ['$provide'];
