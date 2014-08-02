7fe8dce104ae2b28228146642a07a3123af96375
Tero Parviainen <tero@teropa.info>
2014-08-02 18:48:57 +0300

    Registering Directives

--- src/compile.js
+++ src/compile.js
@@ -1,11 +1,36 @@
 'use strict';
 
-function $CompileProvider() {
+var _ = require('lodash');
+
+function $CompileProvider($provide) {
+
+  var hasDirectives = {};
+
+  this.directive = function(name, directiveFactory) {
+    if (_.isString(name)) {
+      if (name === 'hasOwnProperty') {
+        throw 'hasOwnProperty is not a valid directive name';
+      }
+      if (!hasDirectives.hasOwnProperty(name)) {
+        hasDirectives[name] = [];
+        $provide.factory(name + 'Directive', ['$injector', function($injector) {
+          var factories = hasDirectives[name];
+          return _.map(factories, $injector.invoke);
+        }]);
+      }
+      hasDirectives[name].push(directiveFactory);
+    } else {
+      _.forEach(name, _.bind(function(directiveFactory, name) {
+        this.directive(name, directiveFactory);
+      }, this));
+    }
+  };
 
   this.$get = function() {
 
   };
 
 }
+$CompileProvider.$inject = ['$provide'];
 
 module.exports = $CompileProvider;
--- src/loader.js
+++ src/loader.js
@@ -33,6 +33,7 @@ function setupModuleLoader(window) {
       service: invokeLater('$provide', 'service'),
       decorator: invokeLater('$provide', 'decorator'),
       filter: invokeLater('$filterProvider', 'register'),
+      directive: invokeLater('$compileProvider', 'directive'),
       config: invokeLater('$injector', 'invoke', 'push', configBlocks),
       run: function(fn) {
         moduleInstance._runBlocks.push(fn);
