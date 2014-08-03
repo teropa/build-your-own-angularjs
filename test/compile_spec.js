5d7b705108c6181993bc836cae489274419c02c3
Tero Parviainen <tero@teropa.info>
2014-08-03 13:20:26 +0300

    Applying Directives to Comments

--- test/compile_spec.js
+++ test/compile_spec.js
@@ -306,5 +306,21 @@ describe('$compile', function() {
     });
   });
 
+  it('compiles comment directives', function() {
+    var hasCompiled;
+    var injector = makeInjectorWithDirectives('myDirective', function() {
+      return {
+        compile: function(element) {
+          hasCompiled = true;
+        }
+      };
+    });
+    injector.invoke(function($compile) {
+      var el = $('<!-- directive: my-directive -->');
+      $compile(el);
+      expect(hasCompiled).toBe(true);
+    });
+  });
+
 
 });
