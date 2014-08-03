0a08805c1a0fcaa6f3d9f80ddcbc0b0f6a2879a0
Tero Parviainen <tero@teropa.info>
2014-08-03 10:41:33 +0300

    Using Prefixes with Element Directives

--- src/compile.js
+++ src/compile.js
@@ -3,8 +3,14 @@
 var _ = require('lodash');
 var $ = require('jquery');
 
+var PREFIX_REGEXP = /(x[\:\-_]|data[\:\-_])/i;
+
 function nodeName(element) {
   return element.nodeName ? element.nodeName : element[0].nodeName;
+}
+
+function directiveNormalize(name) {
+  return _.camelCase(name.replace(PREFIX_REGEXP, ''));
 }
 
 function $CompileProvider($provide) {
@@ -49,7 +55,7 @@ function $CompileProvider($provide) {
 
     function collectDirectives(node) {
       var directives = [];
-      var normalizedNodeName = _.camelCase(nodeName(node).toLowerCase());
+      var normalizedNodeName = directiveNormalize(nodeName(node).toLowerCase());
       addDirective(directives, normalizedNodeName);
       return directives;
     }
