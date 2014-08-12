d241fd3e6fc827c63a69492f89819bd698f0d084
Tero Parviainen <tero@teropa.info>
2014-08-12 08:11:14 +0300

    Passing Attributes to the compile Function

--- src/compile.js
+++ src/compile.js
@@ -47,15 +47,16 @@ function $CompileProvider($provide) {
 
     function compileNodes($compileNodes) {
       _.forEach($compileNodes, function(node) {
-        var directives = collectDirectives(node);
-        applyDirectivesToNode(directives, node);
+        var attrs = {};
+        var directives = collectDirectives(node, attrs);
+        applyDirectivesToNode(directives, node, attrs);
         if (node.childNodes && node.childNodes.length) {
           compileNodes(node.childNodes);
         }
       });
     }
 
-    function collectDirectives(node) {
+    function collectDirectives(node, attrs) {
       var directives = [];
       if (node.nodeType === Node.ELEMENT_NODE) {
         var normalizedNodeName = directiveNormalize(nodeName(node).toLowerCase());
@@ -78,6 +79,7 @@ function $CompileProvider($provide) {
           }
           normalizedAttrName = directiveNormalize(name.toLowerCase());
           addDirective(directives, normalizedAttrName, 'A', attrStartName, attrEndName);
+          attrs[normalizedAttrName] = attr.value.trim();
         });
         _.forEach(node.classList, function(cls) {
           var normalizedClassName = directiveNormalize(cls);
@@ -107,14 +109,14 @@ function $CompileProvider($provide) {
       }
     }
 
-    function applyDirectivesToNode(directives, compileNode) {
+    function applyDirectivesToNode(directives, compileNode, attrs) {
       var $compileNode = $(compileNode);
       _.forEach(directives, function(directive) {
         if (directive.$$start) {
           $compileNode = groupScan(compileNode, directive.$$start, directive.$$end);
         }
         if (directive.compile) {
-          directive.compile($compileNode);
+          directive.compile($compileNode, attrs);
         }
       });
     }
