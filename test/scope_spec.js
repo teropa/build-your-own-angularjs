845682c8586711a8f69220a6e6f511a363d08b94
Tero Parviainen <tero@teropa.info>
2014-11-15 10:25:17 +0200

    Substituting The Parent Scope

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -1174,6 +1174,26 @@ describe("Scope", function() {
       expect(child.didPostDigest).toBe(true);
     });
 
+    it('can take some other scope as the parent', function() {
+      var prototypeParent = new Scope();
+      var hierarchyParent = new Scope();
+      var child = prototypeParent.$new(false, hierarchyParent);
+
+      prototypeParent.a = 42;
+      expect(child.a).toBe(42);
+
+      child.counter = 0;
+      child.$watch(function(scope) { 
+        scope.counter++;
+      });
+
+      prototypeParent.$digest();
+      expect(child.counter).toBe(0);
+
+      hierarchyParent.$digest();
+      expect(child.counter).toBe(2);
+    });
+
   });
 
 });
