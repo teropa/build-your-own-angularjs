b9a6dedfb83a25d93b1e375ee1fc8cfc407bab40
Tero Parviainen <tero@teropa.info>
2014-11-16 13:55:57 +0200

    Overriding attributes with ng-attr

--- src/compile.js
+++ src/compile.js
@@ -111,7 +111,8 @@ function $CompileProvider($provide) {
           var attrStartName, attrEndName;
           var name = attr.name;
           var normalizedAttrName = directiveNormalize(name.toLowerCase());
-          if (/^ngAttr[A-Z]/.test(normalizedAttrName)) {
+          var isNgAttr = /^ngAttr[A-Z]/.test(normalizedAttrName);
+          if (isNgAttr) {
             name = _.kebabCase(
               normalizedAttrName[6].toLowerCase() +
               normalizedAttrName.substring(7)
@@ -127,9 +128,11 @@ function $CompileProvider($provide) {
           }
           normalizedAttrName = directiveNormalize(name.toLowerCase());
           addDirective(directives, normalizedAttrName, 'A', attrStartName, attrEndName);
-          attrs[normalizedAttrName] = attr.value.trim();
-          if (isBooleanAttribute(node, normalizedAttrName)) {
-            attrs[normalizedAttrName] = true;
+          if (isNgAttr || !attrs.hasOwnProperty(normalizedAttrName)) {
+            attrs[normalizedAttrName] = attr.value.trim();
+            if (isBooleanAttribute(node, normalizedAttrName)) {
+              attrs[normalizedAttrName] = true;
+            }
           }
         });
         _.forEach(node.classList, function(cls) {
