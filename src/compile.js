cad004b4bac584861de9be29e8d437e56d0e1be0
Tero Parviainen <tero@teropa.info>
2014-12-09 06:50:22 +0200

    The Public Link Function

--- src/compile.js
+++ src/compile.js
@@ -141,7 +141,11 @@ function $CompileProvider($provide) {
     };
 
     function compile($compileNodes) {
-      return compileNodes($compileNodes);
+      compileNodes($compileNodes);
+
+      return function publicLinkFn(scope) {
+        $compileNodes.data('$scope', scope);
+      };
     }
 
     function compileNodes($compileNodes) {
