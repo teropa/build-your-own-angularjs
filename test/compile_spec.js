dbb64553d7a6a9ed5d64fb3fb21ed266d991c59c
Tero Parviainen <tero@teropa.info>
2014-12-11 07:37:08 +0200

    Keeping The Node List Stable for Linking

--- test/compile_spec.js
+++ test/compile_spec.js
@@ -932,6 +932,26 @@ describe('$compile', function() {
       });
     });
 
+    it('stabilizes node list during linking', function() {
+      var givenElements = [];
+      var injector = makeInjectorWithDirectives('myDirective', function() {
+        return {
+          link: function(scope, element, attrs) {
+            givenElements.push(element[0]);
+            element.after('<div></div>');
+          }
+        };
+      });
+      injector.invoke(function($compile, $rootScope) {
+        var el = $('<div><div my-directive></div><div my-directive></div></div>');
+        var el1 = el[0].childNodes[0], el2 = el[0].childNodes[1];
+        $compile(el)($rootScope);
+        expect(givenElements.length).toBe(2);
+        expect(givenElements[0]).toBe(el1);
+        expect(givenElements[1]).toBe(el2);
+      });
+    });
+
   });
 
 });
