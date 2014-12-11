1c28019c7661140f9cf433b5dcb9c98265bb0af1
Tero Parviainen <tero@teropa.info>
2014-12-11 08:11:27 +0200

    Linking Directives Across Multiple Nodes

--- test/compile_spec.js
+++ test/compile_spec.js
@@ -952,6 +952,27 @@ describe('$compile', function() {
       });
     });
 
+    it('invokes multi-element directive link functions with whole group', function() {
+      var givenElements;
+      var injector = makeInjectorWithDirectives('myDirective', function() {
+        return {
+          multiElement: true,
+          link: function(scope, element, attrs) {
+            givenElements = element;
+          }
+        };
+      });
+      injector.invoke(function($compile, $rootScope) {
+        var el = $(
+          '<div my-directive-start></div>'+
+          '<p></p>'+
+          '<div my-directive-end></div>'
+        );
+        $compile(el)($rootScope);
+        expect(givenElements.length).toBe(3);
+      });
+    });
+
   });
 
 });
