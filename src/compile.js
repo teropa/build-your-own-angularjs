3b7b57aa3236a308e3117699652e6eadbd80875d
Tero Parviainen <tero@teropa.info>
2014-12-17 07:44:59 +0200

    Isolate Attribute Bindings

--- src/compile.js
+++ src/compile.js
@@ -35,6 +35,18 @@ function isBooleanAttribute(node, attrName) {
   return BOOLEAN_ATTRS[attrName] && BOOLEAN_ELEMENTS[node.nodeName];
 }
 
+function parseIsolateBindings(scope) {
+  var bindings = {};
+  _.forEach(scope, function(definition, scopeName) {
+    var match = definition.match(/\s*@\s*(\w*)\s*/);
+    bindings[scopeName] = {
+      mode: '@',
+      attrName: match[1] || scopeName
+    };
+  });
+  return bindings;
+}
+
 function $CompileProvider($provide) {
 
   var hasDirectives = {};
@@ -54,6 +66,9 @@ function $CompileProvider($provide) {
             directive.priority = directive.priority || 0;
             if (directive.link && !directive.compile) {
               directive.compile = _.constant(directive.link);
+            }
+            if (_.isObject(directive.scope)) {
+              directive.$$isolateBindings = parseIsolateBindings(directive.scope);
             }
             directive.name = directive.name || name;
             directive.index = i;
@@ -388,6 +403,20 @@ function $CompileProvider($provide) {
           isolateScope = scope.$new(true);
           $element.addClass('ng-isolate-scope');
           $element.data('$isolateScope', isolateScope);
+
+          _.forEach(newIsolateScopeDirective.$$isolateBindings, function(definition, scopeName) {
+            var attrName = definition.attrName;
+            switch (definition.mode) {
+              case '@':
+                attrs.$observe(attrName, function(newAttrValue) {
+                  isolateScope[scopeName] = newAttrValue;
+                });
+                if (attrs[attrName]) {
+                  isolateScope[scopeName] = attrs[attrName];
+                }
+                break;
+            }
+          });
         }
 
         _.forEach(preLinkFns, function(linkFn) {
