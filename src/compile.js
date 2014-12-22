edbe55df0699918a65c1aff752d1183395dc0cbd
Tero Parviainen <tero@teropa.info>
2014-12-22 19:17:49 +0200

    Expression Binding

--- src/compile.js
+++ src/compile.js
@@ -36,7 +36,7 @@ function isBooleanAttribute(node, attrName) {
 function parseIsolateBindings(scope) {
   var bindings = {};
   _.forEach(scope, function(definition, scopeName) {
-    var match = definition.match(/\s*(@|=(\*?))\s*(\w*)\s*/);
+    var match = definition.match(/\s*([@&]|=(\*?))\s*(\w*)\s*/);
     bindings[scopeName] = {
       mode: match[1][0],
       collection: match[2] === '*',
@@ -432,6 +432,12 @@ function $CompileProvider($provide) {
                   unwatch = scope.$watch(parentValueWatch);
                 }
                 isolateScope.$on('$destroy', unwatch);
+                break;
+              case '&':
+                var parentExpr = $parse(attrs[attrName]);
+                isolateScope[scopeName] = function(locals) {
+                  return parentExpr(scope, locals);
+                };
                 break;
             }
           });
