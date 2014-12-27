939fae43af0af38e81f1758390254ce90dbad625
Tero Parviainen <tero@teropa.info>
2014-12-27 11:24:41 +0100

    Controller Registration

--- src/controller.js
+++ src/controller.js
@@ -1,10 +1,25 @@
 'use strict';
 
+var _ = require('lodash');
+
 function $ControllerProvider() {
+
+  var controllers = {};
+
+  this.register = function(name, controller) {
+    if (_.isObject(name)) {
+      _.extend(controllers, name);
+    } else {
+      controllers[name] = controller;
+    }
+  };
 
   this.$get = ['$injector', function($injector) {
 
     return function(ctrl, locals) {
+      if (_.isString(ctrl)) {
+        ctrl = controllers[ctrl];
+      }
       return $injector.instantiate(ctrl, locals);
     };
 
