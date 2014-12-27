6bf62950d97cb91eb8fe54b2818cee5175b895dc
Tero Parviainen <tero@teropa.info>
2014-12-27 11:51:19 +0100

    Global Controller Lookup

--- src/controller.js
+++ src/controller.js
@@ -5,6 +5,11 @@ var _ = require('lodash');
 function $ControllerProvider() {
 
   var controllers = {};
+  var globals = false;
+
+  this.allowGlobals = function() {
+    globals = true;
+  };
 
   this.register = function(name, controller) {
     if (_.isObject(name)) {
@@ -18,7 +23,11 @@ function $ControllerProvider() {
 
     return function(ctrl, locals) {
       if (_.isString(ctrl)) {
-        ctrl = controllers[ctrl];
+        if (controllers.hasOwnProperty(ctrl)) {
+          ctrl = controllers[ctrl];
+        } else if (globals) {
+          ctrl = window[ctrl];
+        }
       }
       return $injector.instantiate(ctrl, locals);
     };
