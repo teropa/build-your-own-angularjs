954945906e9bf24a384445ab44863960e69c1f55
Tero Parviainen <tero@teropa.info>
2015-01-26 11:35:55 +0200

    Resolving A Deferred

--- src/q.js
+++ src/q.js
@@ -3,14 +3,32 @@
 
 function $QProvider() {
 
-  this.$get = function() {
+  this.$get = ['$rootScope', function($rootScope) {
+
+    function processQueue(state) {
+      state.pending(state.value);
+    }
+
+    function scheduleProcessQueue(state) {
+      $rootScope.$evalAsync(function() {
+        processQueue(state);
+      });
+    }
 
     function Promise() {
+      this.$$state = {};
     }
+    Promise.prototype.then = function(onFulfilled) {
+      this.$$state.pending = onFulfilled;
+    };
 
     function Deferred() {
       this.promise = new Promise();
     }
+    Deferred.prototype.resolve = function(value) {
+      this.promise.$$state.value = value;
+      scheduleProcessQueue(this.promise.$$state);
+    };
 
     function defer() {
       return new Deferred();
@@ -20,6 +38,6 @@ function $QProvider() {
       defer: defer
     };
 
-  };
+  }];
 
 }
--- test/q_spec.js
+++ test/q_spec.js
@@ -4,11 +4,13 @@
 
 describe("$q", function() {
 
-  var $q;
+  var $q, $rootScope;
 
   beforeEach(function() {
     publishExternalAPI();
-    $q = createInjector(['ng']).get('$q');
+    var injector = createInjector(['ng']);
+    $q = injector.get('$q');
+    $rootScope = injector.get('$rootScope');
   });
 
   it('can create a deferred', function() {
@@ -19,6 +21,57 @@ describe("$q", function() {
   it('has a promise for each Deferred', function() {
     var d = $q.defer();
     expect(d.promise).toBeDefined();
+  });
+
+  it('can resolve a promise', function(done) {
+    var deferred = $q.defer();
+    var promise  = deferred.promise;
+
+    var promiseSpy = jasmine.createSpy();
+    promise.then(promiseSpy);
+
+    deferred.resolve('a-ok');
+
+    setTimeout(function() {
+      expect(promiseSpy).toHaveBeenCalledWith('a-ok');
+      done();
+    }, 1);
+  });
+
+  it('works when resolved before promise listener', function(done) {
+    var d = $q.defer();
+    d.resolve(42);
+
+    var promiseSpy = jasmine.createSpy();
+    d.promise.then(promiseSpy);
+
+    setTimeout(function() {
+      expect(promiseSpy).toHaveBeenCalledWith(42);
+      done();
+    }, 0);
+  });
+
+  it('does not resolve promise immediately', function() {
+    var d = $q.defer();
+
+    var promiseSpy = jasmine.createSpy();
+    d.promise.then(promiseSpy);
+
+    d.resolve(42);
+
+    expect(promiseSpy).not.toHaveBeenCalled();
+  });
+
+  it('resolves promise at next digest', function() {
+    var d = $q.defer();
+
+    var promiseSpy = jasmine.createSpy();
+    d.promise.then(promiseSpy);
+
+    d.resolve(42);
+    $rootScope.$apply();
+
+    expect(promiseSpy).toHaveBeenCalledWith(42);
   });
 
 });
