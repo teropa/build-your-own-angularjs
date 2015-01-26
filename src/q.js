31ace461dc63a03621f473f67510ccb9e8bf74db
Tero Parviainen <tero@teropa.info>
2015-01-26 11:35:55 +0200

    Resolving A Deferred

--- src/q.js
+++ src/q.js
@@ -2,14 +2,32 @@
 
 function $QProvider() {
 
-  this.$get = function() {
+  this.$get = ['$rootScope', function($rootScope) {
+
+    function processQueue(state) {
+      state.pending(state.value);
+    }
+
+    function scheduleProcessQueue(state) {
+      $rootScope.$evalAsync(function() {
+        processQueue(state);
+      });
+    }
 
     function Promise() {
+      this.$$state = {};
     }
+    Promise.prototype.then = function(onFulfilled) {
+      this.$$state.pending = onFulfilled;
+    };
 
     function Deferred() {
       this.promise = new Promise();
     }
+    Deferred.prototype.resolve = function(value) {
+      this.promise.$$state.value = value;
+      scheduleProcessQueue(this.promise.$$state);
+    };
 
     function defer() {
       return new Deferred();
@@ -19,7 +37,7 @@ function $QProvider() {
       defer: defer
     };
 
-  };
+  }];
 
 }
 
