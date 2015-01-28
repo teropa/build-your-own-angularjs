4927c773413264ab158b76359692b19aeaaefeed
Tero Parviainen <tero@teropa.info>
2015-01-28 08:16:44 +0200

    Promise Chaining

--- src/q.js
+++ src/q.js
@@ -10,9 +10,14 @@ function $QProvider() {
       var pending = state.pending;
       state.pending = undefined;
       _.forEach(pending, function(handlers) {
+        var deferred = handlers[0];
         var fn = handlers[state.status];
         if (_.isFunction(fn)) {
-          fn(state.value);
+          deferred.resolve(fn(state.value));
+        } else if (state.status === 1) {
+          deferred.resolve(state.value);
+        } else {
+          deferred.reject(state.value);
         }
       });
     }
@@ -27,11 +32,13 @@ function $QProvider() {
       this.$$state = {};
     }
     Promise.prototype.then = function(onFulfilled, onRejected) {
+      var result = new Deferred();
       this.$$state.pending = this.$$state.pending || [];
-      this.$$state.pending.push([null, onFulfilled, onRejected]);
+      this.$$state.pending.push([result, onFulfilled, onRejected]);
       if (this.$$state.status > 0) {
         scheduleProcessQueue(this.$$state);
       }
+      return result.promise;
     };
     Promise.prototype.catch = function(onRejected) {
       return this.then(null, onRejected);
