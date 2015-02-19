253eb2a85f1f6f51b26b851ed29342bafea0ee45
Tero Parviainen <tero@teropa.info>
2015-02-19 07:44:49 +0200

    Promises Without $digest Integration: $$q

--- src/angular_public.js
+++ src/angular_public.js
@@ -9,7 +9,8 @@ function publishExternalAPI() {
   ngModule.provider('$filter', require('./filter'));
   ngModule.provider('$parse', require('./parse'));
   ngModule.provider('$rootScope', require('./scope'));
-  ngModule.provider('$q', require('./q'));
+  ngModule.provider('$q', require('./q').$QProvider);
+  ngModule.provider('$$q', require('./q').$$QProvider);
 }
 
 module.exports = publishExternalAPI;
