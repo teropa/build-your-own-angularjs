0d337fcc5987b3de2c62d3a0bb5f599206123dc2
Tero Parviainen <tero@teropa.info>
2015-03-22 09:05:33 +0200

    Request Headers

--- src/http_backend.js
+++ src/http_backend.js
@@ -1,11 +1,16 @@
 'use strict';
 
+var _ = require('lodash');
+
 function $HttpBackendProvider() {
 
   this.$get = function() {
-    return function(method, url, post, callback) {
+    return function(method, url, post, callback, headers) {
       var xhr = new window.XMLHttpRequest();
       xhr.open(method, url, true);
+      _.forEach(headers, function(value, key) {
+        xhr.setRequestHeader(key, value);
+      });
       xhr.send(post || null);
       xhr.onload = function() {
         var response = ('response' in xhr) ? xhr.response :
