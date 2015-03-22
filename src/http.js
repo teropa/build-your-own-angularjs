377c59edce21243ad2ad25f1d62ff27d77a257fb
Tero Parviainen <tero@teropa.info>
2015-03-22 10:32:51 +0200

    Request Transforms

--- src/http.js
+++ src/http.js
@@ -60,15 +60,21 @@ function $HttpProvider() {
   }
 
   function parseHeaders(headers) {
-    var lines = headers.split('\n');
-    return _.transform(lines, function(result, line) {
-      var separatorAt = line.indexOf(':');
-      var name = _.trim(line.substr(0, separatorAt)).toLowerCase();
-      var value = _.trim(line.substr(separatorAt + 1));
-      if (name) {
-        result[name] = value;
-      }
-    }, {});
+    if (_.isObject(headers)) {
+      return _.transform(headers, function(result, v, k) {
+        result[_.trim(k.toLowerCase())] = _.trim(v);
+      }, {});
+    } else {
+      var lines = headers.split('\n');
+      return _.transform(lines, function(result, line) {
+        var separatorAt = line.indexOf(':');
+        var name = _.trim(line.substr(0, separatorAt)).toLowerCase();
+        var value = _.trim(line.substr(separatorAt + 1));
+        if (name) {
+          result[name] = value;
+        }
+      }, {});
+    }
   }
 
   function headersGetter(headers) {
@@ -83,6 +89,15 @@ function $HttpProvider() {
     };
   }
 
+  function transformData(data, headers, transform) {
+    if (_.isFunction(transform)) {
+      return transform(data, headers);
+    } else {
+      return _.reduce(transform, function(data, fn) {
+        return fn(data, headers);
+      }, data);
+    }
+  }
 
   this.$get = ['$httpBackend', '$q', '$rootScope', function($httpBackend, $q, $rootScope) {
 
@@ -90,7 +105,8 @@ function $HttpProvider() {
       var deferred = $q.defer();
 
       var config = _.extend({
-        method: 'GET'
+        method: 'GET',
+        transformRequest: defaults.transformRequest
       }, requestConfig);
       config.headers = mergeHeaders(requestConfig);
 
@@ -98,8 +114,14 @@ function $HttpProvider() {
           !_.isUndefined(defaults.withCredentials)) {
         config.withCredentials = defaults.withCredentials;
       }
-      
-      if (_.isUndefined(config.data)) {
+
+      var reqData = transformData(
+        config.data,
+        headersGetter(config.headers),
+        config.transformRequest
+      );
+
+      if (_.isUndefined(reqData)) {
         _.forEach(config.headers, function(v, k) {
           if (k.toLowerCase() === 'content-type') {
             delete config.headers[k];
@@ -124,7 +146,7 @@ function $HttpProvider() {
       $httpBackend(
         config.method,
         config.url,
-        config.data,
+        reqData,
         done,
         config.headers,
         config.withCredentials
