e21685417bd07a0bc8f74c8f61402dc9215b123c
Tero Parviainen <tero@teropa.info>
2015-03-23 08:12:34 +0200

    JSON Serialization And Parsing

--- src/http.js
+++ src/http.js
@@ -6,6 +6,35 @@ function isSuccess(status) {
   return status >= 200 && status < 300;
 }
 
+function isBlob(object) {
+  return object.toString() === '[object Blob]';
+}
+function isFile(object) {
+  return object.toString() === '[object File]';
+}
+function isFormData(object) {
+  return object.toString() === '[object FormData]';
+}
+
+function isJsonLike(data) {
+  if (data.match(/^\{(?!\{)/)) {
+    return data.match(/\}$/);
+  } else if (data.match(/^\[/)) {
+    return data.match(/\]$/);
+  }
+}
+
+function defaultHttpResponseTransform(data, headers) {
+  if (_.isString(data)) {
+    var contentType = headers('Content-Type');
+    if ((contentType && contentType.indexOf('application/json') === 0) ||
+        isJsonLike(data)) {
+      return JSON.parse(data);
+    }
+  }
+  return data;
+}
+
 function $HttpProvider() {
 
   var defaults = this.defaults = {
@@ -22,7 +51,16 @@ function $HttpProvider() {
       patch: {
         'Content-Type': 'application/json;charset=utf-8'
       }
-    }
+    },
+    transformRequest: [function(data) {
+      if (_.isObject(data) && !isBlob(data) &&
+          !isFile(data) && !isFormData(data)) {
+        return JSON.stringify(data);
+      } else {
+        return data;
+      }
+    }],
+    transformResponse: [defaultHttpResponseTransform]
   };
 
   function executeHeaderFns(headers, config) {
@@ -133,7 +171,8 @@ function $HttpProvider() {
     function $http(requestConfig) {
       var config = _.extend({
         method: 'GET',
-        transformRequest: defaults.transformRequest
+        transformRequest: defaults.transformRequest,
+        transformResponse: defaults.transformResponse
       }, requestConfig);
       config.headers = mergeHeaders(requestConfig);
 
