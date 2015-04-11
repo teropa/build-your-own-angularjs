f9f2dd26d56dcf0f771674d643f5284f6de62428
Tero Parviainen <tero@teropa.info>
2015-04-11 10:11:45 +0300

    Disallowing More Than One Template Directive Per Element

--- src/compile.js
+++ src/compile.js
@@ -401,6 +401,7 @@ function $CompileProvider($provide) {
       var terminal = false;
       var preLinkFns = [], postLinkFns = [], controllers = {};
       var newScopeDirective, newIsolateScopeDirective;
+      var templateDirective;
       var controllerDirectives;
 
       function getControllers(require, $element) {
@@ -496,7 +497,10 @@ function $CompileProvider($provide) {
           controllerDirectives[directive.name] = directive;
         }
         if (directive.template) {
-          $compileNode.html(directive.template);
+          if (templateDirective) {
+            throw 'Multiple directives asking for template';
+          }
+          templateDirective = directive;
         }
         if (directive.terminal) {
           terminal = true;
--- test/compile_spec.js
+++ test/compile_spec.js
@@ -2329,6 +2329,23 @@ describe('$compile', function() {
       });
     });
 
+    it('does not allow two directives with templates', function() {
+      var injector = makeInjectorWithDirectives({
+        myDirective: function() {
+          return {template: '<div></div>'};
+        },
+        myOtherDirective: function() {
+          return {template: '<div></div>'};
+        }
+      });
+      injector.invoke(function($compile) {
+        var el = $('<div my-directive my-other-directive></div>');
+        expect(function() {
+          $compile(el);
+        }).toThrow();
+      });
+    });
+
   });
 
 });
