ad2995b556ee0a756475118f9310cca08b517fd2
Tero Parviainen <tero@teropa.info>
2015-04-11 10:11:56 +0300

    Template Functions

--- test/compile_spec.js
+++ test/compile_spec.js
@@ -2553,6 +2553,25 @@ describe('$compile', function() {
       });
     });
 
+    it('supports functions as template values', function() {
+      var templateSpy = jasmine.createSpy()
+        .and.returnValue('<div class="from-template"></div>');
+      var injector = makeInjectorWithDirectives({
+        myDirective: function() {
+          return {
+            template: templateSpy
+          };
+        }
+      });
+      injector.invoke(function($compile) {
+        var el = $('<div my-directive></div>');
+        $compile(el);
+        expect(el.find('> .from-template').length).toBe(1);
+        expect(templateSpy.calls.first().args[0][0]).toBe(el[0]);
+        expect(templateSpy.calls.first().args[1].myDirective).toBeDefined();
+      });
+    });
+
   });
 
 });
