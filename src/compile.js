09fe882ebff9c63655b6e6c390c856b14ee93ffb
Tero Parviainen <tero@teropa.info>
2015-04-27 10:23:12 +0300

    Preserving Controller Directives

--- src/compile.js
+++ src/compile.js
@@ -368,7 +368,7 @@ function $CompileProvider($provide) {
       var newScopeDirective;
       var newIsolateScopeDirective = previousCompileContext.newIsolateScopeDirective;
       var templateDirective = previousCompileContext.templateDirective;
-      var controllerDirectives;
+      var controllerDirectives = previousCompileContext.controllerDirectives;
 
       function getControllers(require, $element) {
         if (_.isArray(require)) {
@@ -471,6 +471,7 @@ function $CompileProvider($provide) {
             {
               templateDirective: templateDirective,
               newIsolateScopeDirective: newIsolateScopeDirective,
+              controllerDirectives: controllerDirectives,
               preLinkFns: preLinkFns,
               postLinkFns: postLinkFns
             }
