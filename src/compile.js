8b2b3516588e26cda2cf412925e62b96cd15c946
Tero Parviainen <tero@teropa.info>
2015-06-01 08:34:10 +0100

    Transclusion in Controllers

--- src/compile.js
+++ src/compile.js
@@ -649,6 +649,7 @@ function $CompileProvider($provide) {
             var locals = {
               $scope: directive === newIsolateScopeDirective ? isolateScope : scope,
               $element: $element,
+              $transclude: scopeBoundTranscludeFn,
               $attrs: attrs
             };
             var controllerName = directive.controller;
