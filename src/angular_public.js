6b824edb8410f889d77e77dd63b6276f21d42e45
Tero Parviainen <tero@teropa.info>
2015-06-04 19:06:50 +0100

    The ngTransclude Directive

--- src/angular_public.js
+++ src/angular_public.js
@@ -18,6 +18,7 @@ function publishExternalAPI() {
   ngModule.provider('$compile', require('./compile'));
   ngModule.provider('$controller', require('./controller'));
   ngModule.directive('ngController', require('./directives/ng_controller'));
+  ngModule.directive('ngTransclude', require('./directives/ng_transclude'));
 }
 
 module.exports = publishExternalAPI;
