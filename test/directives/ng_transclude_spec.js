242911227c67f8880785003181d3ffc1e4d088e9
Tero Parviainen <tero@teropa.info>
2015-06-04 19:06:50 +0100

    The ngTransclude Directive

---
+++ test/directives/ng_transclude_spec.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var $ = require('jquery');
+var publishExternalAPI = require('../../src/angular_public');
+var createInjector = require('../../src/injector');
+
+describe('ngTransclude', function() {
+
+  beforeEach(function() {
+    delete window.angular;
+    publishExternalAPI();
+  });
+
+  function createInjectorWithTranscluderTemplate(template) {
+    return createInjector(['ng', function($compileProvider) {
+      $compileProvider.directive('myTranscluder', function() {
+        return {
+          transclude: true,
+          template: template
+        };
+      });
+    }]);
+  }
+
+  it('transcludes the parent directive transclusion', function() {
+    var injector = createInjectorWithTranscluderTemplate(
+      '<div ng-transclude></div>'
+    );
+    injector.invoke(function($compile, $rootScope) {
+      var el = $('<div my-transcluder>Hello</div>');
+      $compile(el)($rootScope);
+      expect(el.find('> [ng-transclude]').html()).toEqual('Hello');
+    });
+  });
+
+  it('empties existing contents', function() {
+    var injector = createInjectorWithTranscluderTemplate(
+      '<div ng-transclude>Existing contents</div>'
+    );
+    injector.invoke(function($compile, $rootScope) {
+      var el = $('<div my-transcluder>Hello</div>');
+      $compile(el)($rootScope);
+      expect(el.find('> [ng-transclude]').html()).toEqual('Hello');
+    });
+  });
+
+  it('may be used as element', function() {
+    var injector = createInjectorWithTranscluderTemplate(
+      '<ng-transclude>Existing contents</ng-transclude>'
+    );
+    injector.invoke(function($compile, $rootScope) {
+      var el = $('<div my-transcluder>Hello</div>');
+      $compile(el)($rootScope);
+      expect(el.find('> ng-transclude').html()).toEqual('Hello');
+    });
+  });
+
+  it('may be used as class', function() {
+    var injector = createInjectorWithTranscluderTemplate(
+      '<div class="ng-transclude">Existing contents</div>'
+    );
+    injector.invoke(function($compile, $rootScope) {
+      var el = $('<div my-transcluder>Hello</div>');
+      $compile(el)($rootScope);
+      expect(el.find('> .ng-transclude').html()).toEqual('Hello');
+    });
+  });
+
+});
