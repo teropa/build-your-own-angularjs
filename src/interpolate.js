5c0821e06378dc231c11afbd7cec666bf0afbf9f
Tero Parviainen <tero@teropa.info>
2015-06-11 06:00:21 +0200

    Interpolating Strings

--- src/interpolate.js
+++ src/interpolate.js
@@ -3,12 +3,44 @@
 
 function $InterpolateProvider() {
 
-  this.$get = function() {
+  this.$get = ['$parse', function($parse) {
 
-    function $interpolate() {
+    function $interpolate(text) {
+      var index = 0;
+      var parts = [];
+      var startIndex, endIndex, exp, expFn;
+      while (index < text.length) {
+        startIndex = text.indexOf('{{', index);
+        if (startIndex !== -1) {
+          endIndex = text.indexOf('}}', startIndex + 2);
+        }
+        if (startIndex !== -1 && endIndex !== -1) {
+          if (startIndex !== index) {
+            parts.push(text.substring(index, startIndex));
+          }
+          exp = text.substring(startIndex + 2, endIndex);
+          expFn = $parse(exp);
+          parts.push(expFn);
+          index = endIndex + 2;
+        } else {
+          parts.push(text.substring(index));
+          break;
+        }
+      }
+
+      return function interpolationFn(context) {
+        return _.reduce(parts, function(result, part) {
+          if (_.isFunction(part)) {
+            return result + part(context);
+          } else {
+            return result + part;
+          }
+        }, '');
+      };
+
     }
 
     return $interpolate;
-  };
+  }];
 
 }
--- test/interpolate_spec.js
+++ test/interpolate_spec.js
@@ -10,4 +10,37 @@ describe('$interpolate', function() {
     expect(injector.has('$interpolate')).toBe(true);
   });
 
+  it('produces an identity function for static content', function() {
+    var injector = createInjector(['ng']);
+    var $interpolate = injector.get('$interpolate');
+
+    var interp = $interpolate('hello');
+    expect(interp instanceof Function).toBe(true);
+    expect(interp()).toEqual('hello');
+  });
+
+  it('evaluates a single expression', function() {
+    var injector = createInjector(['ng']);
+    var $interpolate = injector.get('$interpolate');
+
+    var interp = $interpolate('{{anAttr}}');
+    expect(interp({anAttr: '42'})).toEqual('42');
+  });
+
+  it('evaluates many expressions', function() {
+    var injector = createInjector(['ng']);
+    var $interpolate = injector.get('$interpolate');
+
+    var interp = $interpolate('First {{anAttr}}, then {{anotherAttr}}!');
+    expect(interp({anAttr: '42', anotherAttr: '43'})).toEqual('First 42, then 43!');
+  });
+
+  it('passes through ill-defined interpolations', function() {
+    var injector = createInjector(['ng']);
+    var $interpolate = injector.get('$interpolate');
+
+    var interp = $interpolate('why u no }}work{{');
+    expect(interp({})).toEqual('why u no }}work{{');
+  });
+
 });
