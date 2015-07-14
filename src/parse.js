772ad314f434c4d2af94fa2fdbc1c19a103c0ab8
Tero Parviainen <tero@teropa.info>
2015-07-14 13:51:54 +0300

    Parsing Floating Point Numbers

--- src/parse.js
+++ src/parse.js
@@ -12,7 +12,8 @@ Lexer.prototype.lex = function(text) {
 
   while (this.index < this.text.length) {
     this.ch = this.text.charAt(this.index);
-    if (this.isNumber(this.ch)) {
+    if (this.isNumber(this.ch) ||
+         (this.ch === '.' && this.isNumber(this.peek()))) {
       this.readNumber();
     } else {
       throw 'Unexpected next character: ' + this.ch;
@@ -30,7 +31,7 @@ Lexer.prototype.readNumber = function() {
   var number = '';
   while (this.index < this.text.length) {
     var ch = this.text.charAt(this.index);
-    if (this.isNumber(ch)) {
+    if (ch === '.' || this.isNumber(ch)) {
       number += ch;
     } else {
       break;
@@ -42,6 +43,12 @@ Lexer.prototype.readNumber = function() {
     text: number,
     value: Number(number)
   });
+};
+
+Lexer.prototype.peek = function() {
+  return this.index < this.text.length - 1 ?
+    this.text.charAt(this.index + 1) :
+    false;
 };
 
 
--- test/parse_spec.js
+++ test/parse_spec.js
@@ -1,1 +1,23 @@
-/* jshint globalstrict: true *//* global parse: false */'use strict';describe("parse", function() {  it("can parse an integer", function() {    var fn = parse('42');    expect(fn).toBeDefined();    console.log(fn.toString());    expect(fn()).toBe(42);  });});
+/* jshint globalstrict: true */
+/* global parse: false */
+'use strict';
+
+describe("parse", function() {
+
+  it("can parse an integer", function() {
+    var fn = parse('42');
+    expect(fn).toBeDefined();
+    expect(fn()).toBe(42);
+  });
+
+  it("can parse a floating point number", function() {
+    var fn = parse('4.2');
+    expect(fn()).toBe(4.2);
+  });
+
+  it("can parse a floating point number without an integer part", function() {
+    var fn = parse('.42');
+    expect(fn()).toBe(0.42);
+  });
+
+});
