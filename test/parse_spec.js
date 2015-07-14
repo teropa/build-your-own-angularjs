0183b64f6dac079fa65f433f45b7e0ba7c9ca36e
Tero Parviainen <tero@teropa.info>
2015-07-14 12:59:42 +0300

    Parsing Integers

---
+++ test/parse_spec.js
@@ -0,0 +1,12 @@
+'use strict';
+var parse = require('../src/parse');
+
+describe('parse', function() {
+
+  it('can parse an integer', function() {
+    var fn = parse('42');
+    expect(fn).toBeDefined();
+    expect(fn()).toBe(42);
+  });
+
+});
