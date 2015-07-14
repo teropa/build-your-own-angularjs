66137f0bf71e7ef642ae348aa49f045e3fbe3741
Tero Parviainen <tero@teropa.info>
2015-07-14 13:51:54 +0300

    Parsing Floating Point Numbers

--- test/parse_spec.js
+++ test/parse_spec.js
@@ -1,4 +1,5 @@
 'use strict';
+
 var parse = require('../src/parse');
 
 describe("parse", function() {
@@ -7,6 +8,16 @@ describe("parse", function() {
     var fn = parse('42');
     expect(fn).toBeDefined();
     expect(fn()).toBe(42);
+  });
+
+  it("can parse a floating point number", function() {
+    var fn = parse('4.2');
+    expect(fn()).toBe(4.2);
+  });
+
+  it("can parse a floating point number without an integer part", function() {
+    var fn = parse('.42');
+    expect(fn()).toBe(0.42);
   });
 
 });
