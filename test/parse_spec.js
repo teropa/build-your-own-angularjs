dfa73119a7dc9e990ec5b1398707a653773526b8
Tero Parviainen <tero@teropa.info>
2015-07-14 14:01:35 +0300

    Parsing Scientific Notation

--- test/parse_spec.js
+++ test/parse_spec.js
@@ -20,4 +20,34 @@ describe("parse", function() {
     expect(fn()).toBe(0.42);
   });
 
+  it("can parse a number in scientific notation", function() {
+    var fn = parse('42e3');
+    expect(fn()).toBe(42000);
+  });
+
+  it("can parse scientific notation with a float coefficient", function() {
+    var fn = parse('.42e2');
+    expect(fn()).toBe(42);
+  });
+
+  it("can parse scientific notation with negative exponents", function() {
+    var fn = parse('4200e-2');
+    expect(fn()).toBe(42);
+  });
+
+  it("can parse scientific notation with the + sign", function() {
+    var fn = parse('.42e+2');
+    expect(fn()).toBe(42);
+  });
+
+  it("can parse upper case scientific notation", function() {
+    var fn = parse('.42E2');
+    expect(fn()).toBe(42);
+  });
+
+  it("will not parse invalid scientific notation", function() {
+    expect(function() { parse('42e-'); }).toThrow();
+    expect(function() { parse('42e-a'); }).toThrow();
+  });
+
 });
