578a96a7932165e87affc8589e1237fdf62ad46f
Tero Parviainen <tero@teropa.info>
2015-07-15 09:58:37 +0300

    Parsing true, false, and null

--- src/parse.js
+++ src/parse.js
@@ -19,6 +19,8 @@ Lexer.prototype.lex = function(text) {
       this.readNumber();
     } else if (this.ch === '\'' || this.ch === '"') {
       this.readString(this.ch);
+    } else if (this.isIdent(this.ch)) {
+      this.readIdent();
     } else {
       throw 'Unexpected next character: ' + this.ch;
     }
@@ -32,6 +34,10 @@ Lexer.prototype.isNumber = function(ch) {
 };
 Lexer.prototype.isExpOperator = function(ch) {
   return ch === '-' || ch === '+' || this.isNumber(ch);
+};
+Lexer.prototype.isIdent = function(ch) {
+  return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') ||
+         ch === '_' || ch === '$';
 };
 
 Lexer.prototype.readNumber = function() {
@@ -103,6 +109,23 @@ Lexer.prototype.readString = function(quote) {
   }
 };
 
+Lexer.prototype.readIdent = function() {
+  var text = '';
+  while (this.index < this.text.length) {
+    var ch = this.text.charAt(this.index);
+    if (this.isIdent(ch) || this.isNumber(ch)) {
+      text += ch;
+    } else {
+      break;
+    }
+    this.index++;
+  }
+
+  var token = {text: text};
+
+  this.tokens.push(token);
+};
+
 Lexer.prototype.peek = function() {
   return this.index < this.text.length - 1 ?
     this.text.charAt(this.index + 1) :
@@ -121,10 +144,25 @@ AST.prototype.ast = function(text) {
   return this.program();
 };
 AST.prototype.program = function() {
-  return {type: AST.Program, body: this.constant()};
+  return {type: AST.Program, body: this.primary()};
+};
+AST.prototype.primary = function() {
+  if (this.constants.hasOwnProperty(this.tokens[0].text)) {
+    return this.constants[this.tokens[0].text];
+  } else {
+    return this.constant();
+  }
 };
 AST.prototype.constant = function() {
   return {type: AST.Literal, value: this.tokens[0].value};
+};
+AST.prototype.constant = function() {
+  return {type: AST.Literal, value: this.tokens[0].value};
+};
+AST.prototype.constants = {
+  'null': {type: AST.Literal, value: null},
+  'true': {type: AST.Literal, value: true},
+  'false': {type: AST.Literal, value: false}
 };
 
 function ASTCompiler(astBuilder) {
@@ -156,6 +194,8 @@ ASTCompiler.prototype.stringEscapeFn = function(c) {
 ASTCompiler.prototype.escape = function(value) {
   if (_.isString(value)) {
     return '\'' + value.replace(this.stringEscapeRegex, this.stringEscapeFn) + '\'';
+  } else if (_.isNull(value)) {
+    return 'null';
   } else {
     return value;
   }
--- test/parse_spec.js
+++ test/parse_spec.js
@@ -82,4 +82,19 @@ describe("parse", function() {
     expect(function() { parse('"\\u00T0"'); }).toThrow();
   });
 
+  it("will parse null", function() {
+    var fn = parse('null');
+    expect(fn()).toBe(null);
+  });
+
+  it("will parse true", function() {
+    var fn = parse('true');
+    expect(fn()).toBe(true);
+  });
+
+  it("will parse false", function() {
+    var fn = parse('false');
+    expect(fn()).toBe(false);
+  });
+
 });
