656929a2cde50192eea377e0f64d4cf5ee278220
Tero Parviainen <tero@teropa.info>
2015-07-17 06:33:31 +0300

    Non-Computed Attribute Lookup

--- test/parse_spec.js
+++ test/parse_spec.js
@@ -150,4 +150,24 @@ describe("parse", function() {
     expect(fn()).toBeUndefined();
   });
 
+  it('looks up a 2-part identifier path from the scope', function() {
+    var fn = parse('aKey.anotherKey');
+    expect(fn({aKey: {anotherKey: 42}})).toBe(42);
+    expect(fn({aKey: {}})).toBeUndefined();
+    expect(fn({})).toBeUndefined();
+  });
+
+  it('looks up a member from an object', function() {
+    var fn = parse('{aKey: 42}.aKey');
+    expect(fn()).toBe(42);
+  });
+
+  it('looks up a 4-part identifier path from the scope', function() {
+    var fn = parse('aKey.secondKey.thirdKey.fourthKey');
+    expect(fn({aKey: {secondKey: {thirdKey: {fourthKey: 42}}}})).toBe(42);
+    expect(fn({aKey: {secondKey: {thirdKey: {}}}})).toBeUndefined();
+    expect(fn({aKey: {}})).toBeUndefined();
+    expect(fn()).toBeUndefined();
+  });
+
 });
