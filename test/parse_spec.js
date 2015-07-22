eef678a281bbe94acb8ab76473f1a785f049ab3b
Tero Parviainen <tero@teropa.info>
2015-07-22 12:05:24 +0300

    Computed Attribute Lookup

--- test/parse_spec.js
+++ test/parse_spec.js
@@ -191,4 +191,24 @@ describe("parse", function() {
     expect(fn(scope, locals)).toBeUndefined();
   });
 
+  it('parses a simple computed property access', function() {
+    var fn = parse('aKey["anotherKey"]');
+    expect(fn({aKey: {anotherKey: 42}})).toBe(42);
+  });
+
+  it('parses a computed numeric array access', function() {
+    var fn = parse('anArray[1]');
+    expect(fn({anArray: [1, 2, 3]})).toBe(2);
+  });
+
+  it('parses a computed access with another key as property', function() {
+    var fn = parse('lock[key]');
+    expect(fn({key: 'theKey', lock: {theKey: 42}})).toBe(42);
+  });
+
+  it('parses computed access with another access as property', function() {
+    var fn = parse('lock[keys["aKey"]]');
+    expect(fn({keys: {aKey: 'theKey'},  lock: {theKey: 42}})).toBe(42);
+  });
+
 });
