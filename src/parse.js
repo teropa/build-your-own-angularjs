01a6c178d5298d4f45ea1e7a7f8ab2d5a3c8af98
Tero Parviainen <tero@teropa.info>
2015-07-22 16:21:26 +0300

    Ensuring Safe Objects

--- src/parse.js
+++ src/parse.js
@@ -12,6 +12,22 @@ function ensureSafeMemberName(name) {
   }
 }
 
+function ensureSafeObject(obj) {
+  if (obj) {
+    if (obj.window === window) {
+      throw 'Referencing window in Angular expressions is disallowed!';
+    } else if (obj.children &&
+                (obj.nodeName || (obj.prop && obj.attr && obj.find))) {
+      throw 'Referencing DOM nodes in Angular expressions is disallowed!';
+    } else if (obj.constructor === obj) {
+      throw 'Referencing Function in Angular expressions is disallowed!';
+    } else if (obj === Object) {
+      throw 'Referencing Object in Angular expressions is disallowed!';
+    }
+  }
+  return obj;
+}
+
 function Lexer() {
 }
 
@@ -322,7 +338,12 @@ ASTCompiler.prototype.compile = function(text) {
     this.state.body.join('') +
     '}; return fn;';
   /* jshint -W054 */
-  return new Function('ensureSafeMemberName', fnString)(ensureSafeMemberName);
+  return new Function(
+    'ensureSafeMemberName',
+    'ensureSafeObject',
+    fnString)(
+      ensureSafeMemberName,
+      ensureSafeObject);
   /* jshint +W054 */
 };
 ASTCompiler.prototype.recurse = function(ast, context, create) {
@@ -365,6 +386,7 @@ ASTCompiler.prototype.recurse = function(ast, context, create) {
       context.name = ast.name;
       context.computed = false;
     }
+    this.addEnsureSafeObject(intoId);
     return intoId;
   case AST.ThisExpression:
     return 's';
@@ -384,7 +406,8 @@ ASTCompiler.prototype.recurse = function(ast, context, create) {
           this.assign(this.computedMember(left, right), '{}'));
       }
       this.if_(left,
-        this.assign(intoId, this.computedMember(left, right)));
+        this.assign(intoId,
+          'ensureSafeObject(' + this.computedMember(left, right) + ')'));
       if (context) {
         context.name = right;
         context.computed = true;
@@ -396,7 +419,8 @@ ASTCompiler.prototype.recurse = function(ast, context, create) {
           this.assign(this.nonComputedMember(left, ast.property.name), '{}'));
       }
       this.if_(left,
-        this.assign(intoId, this.nonComputedMember(left, ast.property.name)));
+        this.assign(intoId,
+          'ensureSafeObject(' + this.nonComputedMember(left, ast.property.name) + ')'));
       if (context) {
         context.name = ast.property.name;
         context.computed = false;
@@ -406,15 +430,18 @@ ASTCompiler.prototype.recurse = function(ast, context, create) {
   case AST.CallExpression:
     var callContext = {};
     var callee = this.recurse(ast.callee, callContext);
-    var args = _.map(ast.arguments, _.bind(this.recurse, this));
+    var args = _.map(ast.arguments, _.bind(function(arg) {
+      return 'ensureSafeObject(' + this.recurse(arg) + ')';
+    }, this));
     if (callContext.name) {
+      this.addEnsureSafeObject(callContext.context);
       if (callContext.computed) {
         callee = this.computedMember(callContext.context, callContext.name);
       } else {
         callee = this.nonComputedMember(callContext.context, callContext.name);
       }
     }
-    return callee + '&&' + callee + '(' + args.join(',') + ')';
+    return callee + '&&ensureSafeObject(' + callee + '(' + args.join(',') + '))';
   case AST.AssignmentExpression:
     var leftContext = {};
     this.recurse(ast.left, leftContext, true);
@@ -424,7 +451,8 @@ ASTCompiler.prototype.recurse = function(ast, context, create) {
     } else {
       leftExpr = this.nonComputedMember(leftContext.context, leftContext.name);
     }
-    return this.assign(leftExpr, this.recurse(ast.right));
+    return this.assign(leftExpr,
+      'ensureSafeObject(' + this.recurse(ast.right) + ')');
   }
 };
 
@@ -453,6 +481,9 @@ ASTCompiler.prototype.assign = function(id, value) {
 };
 ASTCompiler.prototype.addEnsureSafeMemberName = function(expr) {
   this.state.body.push('ensureSafeMemberName(' + expr + ');');
+};
+ASTCompiler.prototype.addEnsureSafeObject = function(expr) {
+  this.state.body.push('ensureSafeObject(' + expr + ');');
 };
 
 ASTCompiler.prototype.stringEscapeRegex = /[^ a-zA-Z0-9]/g;
