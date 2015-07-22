5ff44e9d1a1317fbef6d8fcb876fe25c1793a7a5
Tero Parviainen <tero@teropa.info>
2015-07-22 11:33:34 +0300

    Locals

--- src/parse.js
+++ src/parse.js
@@ -265,7 +265,7 @@ ASTCompiler.prototype.compile = function(text) {
   this.state = {body: [], nextId: 0, vars: []};
   this.recurse(ast);
   /* jshint -W054 */
-  return new Function('s',
+  return new Function('s', 'l',
     (this.state.vars.length ?
       'var ' + this.state.vars.join(',') + ';' :
       ''
@@ -296,7 +296,10 @@ ASTCompiler.prototype.recurse = function(ast) {
     return '{' + properties.join(',') + '}';
   case AST.Identifier:
     intoId = this.nextId();
-    this.if_('s', this.assign(intoId, this.nonComputedMember('s', ast.name)));
+    this.if_(this.getHasOwnProperty('l', ast.name),
+      this.assign(intoId, this.nonComputedMember('l', ast.name)));
+    this.if_(this.not(this.getHasOwnProperty('l', ast.name)) + ' && s',
+      this.assign(intoId, this.nonComputedMember('s', ast.name)));
     return intoId;
   case AST.ThisExpression:
     return 's';
@@ -313,8 +316,14 @@ ASTCompiler.prototype.nextId = function() {
   this.state.vars.push(id);
   return id;
 };
+ASTCompiler.prototype.not = function(e) {
+  return '!(' + e + ')';
+};
 ASTCompiler.prototype.nonComputedMember = function(left, right) {
   return '(' + left + ').' + right;
+};
+ASTCompiler.prototype.getHasOwnProperty = function(object, property) {
+  return object + '&&(' + this.escape(property) + ' in ' + object + ')';
 };
 ASTCompiler.prototype.if_ = function(test, consequent) {
   this.state.body.push('if(', test, '){', consequent, '}');
