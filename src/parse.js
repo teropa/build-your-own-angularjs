ae84d6ae42db88682a0e2d705b93a5501178672e
Tero Parviainen <tero@teropa.info>
2015-07-22 12:41:58 +0300

    Function Calls

--- src/parse.js
+++ src/parse.js
@@ -20,7 +20,7 @@ Lexer.prototype.lex = function(text) {
       this.readNumber();
     } else if (this.is('\'"')) {
       this.readString(this.ch);
-    } else if (this.is('[],{}:.')) {
+    } else if (this.is('[],{}:.()')) {
       this.tokens.push({
         text: this.ch
       });
@@ -163,6 +163,7 @@ AST.Identifier = 'Identifier';
 AST.ThisExpression = 'ThisExpression';
 AST.LocalsExpression = 'LocalsExpression';
 AST.MemberExpression = 'MemberExpression';
+AST.CallExpression = 'CallExpression';
 
 AST.prototype.ast = function(text) {
   this.tokens = this.lexer.lex(text);
@@ -185,7 +186,7 @@ AST.prototype.primary = function() {
     primary = this.constant();
   }
   var next;
-  while ((next = this.expect('.', '['))) {
+  while ((next = this.expect('.', '[', '('))) {
     if (next.text === '[') {
       primary = {
         type: AST.MemberExpression,
@@ -194,13 +195,20 @@ AST.prototype.primary = function() {
         computed: true
       };
       this.consume(']');
-    } else {
+    } else if (next.text === '.') {
       primary = {
         type: AST.MemberExpression,
         object: primary,
         property: this.identifier(),
         computed: false
       };
+    } else if (next.text === '(') {
+      primary = {
+        type: AST.CallExpression,
+        callee: primary,
+        arguments: this.parseArguments()
+      };
+      this.consume(')');
     }
   }
   return primary;
@@ -235,6 +243,15 @@ AST.prototype.object = function() {
   }
   this.consume('}');
   return {type: AST.ObjectExpression, properties: properties};
+};
+AST.prototype.parseArguments = function() {
+  var args = [];
+  if (!this.peek(')')) {
+    do {
+      args.push(this.primary());
+    } while (this.expect(','));
+  }
+  return args;
 };
 AST.prototype.identifier = function() {
   return {type: AST.Identifier, name: this.consume().text};
@@ -333,6 +350,10 @@ ASTCompiler.prototype.recurse = function(ast) {
         this.assign(intoId, this.nonComputedMember(left, ast.property.name)));
     }
     return intoId;
+  case AST.CallExpression:
+    var callee = this.recurse(ast.callee);
+    var args = _.map(ast.arguments, _.bind(this.recurse, this));
+    return callee + '&&' + callee + '(' + args.join(',') + ')';
   }
 };
 
