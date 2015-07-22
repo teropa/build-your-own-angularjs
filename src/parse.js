501b2604e6c06cfa0bb250cc83f959adacc893fd
Tero Parviainen <tero@teropa.info>
2015-07-22 15:42:54 +0300

    Ensuring Safety In Member Access

--- src/parse.js
+++ src/parse.js
@@ -4,6 +4,14 @@ var _ = require('lodash');
 
 var ESCAPES = {'n':'\n', 'f':'\f', 'r':'\r', 't':'\t', 'v':'\v', '\'':'\'', '"':'"'};
 
+function ensureSafeMemberName(name) {
+  if (name === 'constructor' || name === '__proto__' ||
+      name === '__defineGetter__' || name === '__defineSetter__' ||
+      name === '__lookupGetter__' || name === '__lookupSetter__') {
+    throw 'Attempting to access a disallowed field in Angular expressions!';
+  }
+}
+
 function Lexer() {
 }
 
@@ -306,12 +314,15 @@ ASTCompiler.prototype.compile = function(text) {
   var ast = this.astBuilder.ast(text);
   this.state = {body: [], nextId: 0, vars: []};
   this.recurse(ast);
-  /* jshint -W054 */
-  return new Function('s', 'l',
+  var fnString = 'var fn=function(s,l){' +
     (this.state.vars.length ?
       'var ' + this.state.vars.join(',') + ';' :
       ''
-    ) + this.state.body.join(''));
+    ) +
+    this.state.body.join('') +
+    '}; return fn;';
+  /* jshint -W054 */
+  return new Function('ensureSafeMemberName', fnString)(ensureSafeMemberName);
   /* jshint +W054 */
 };
 ASTCompiler.prototype.recurse = function(ast, context, create) {
@@ -337,6 +348,7 @@ ASTCompiler.prototype.recurse = function(ast, context, create) {
     }, this));
     return '{' + properties.join(',') + '}';
   case AST.Identifier:
+    ensureSafeMemberName(ast.name);
     intoId = this.nextId();
     this.if_(this.getHasOwnProperty('l', ast.name),
       this.assign(intoId, this.nonComputedMember('l', ast.name)));
@@ -366,6 +378,7 @@ ASTCompiler.prototype.recurse = function(ast, context, create) {
     }
     if (ast.computed) {
       var right = this.recurse(ast.property);
+      this.addEnsureSafeMemberName(right);
       if (create) {
         this.if_(this.not(this.computedMember(left, right)),
           this.assign(this.computedMember(left, right), '{}'));
@@ -377,6 +390,7 @@ ASTCompiler.prototype.recurse = function(ast, context, create) {
         context.computed = true;
       }
     } else {
+      ensureSafeMemberName(ast.property.name);
       if (create) {
         this.if_(this.not(this.nonComputedMember(left, ast.property.name)),
           this.assign(this.nonComputedMember(left, ast.property.name), '{}'));
@@ -436,6 +450,9 @@ ASTCompiler.prototype.if_ = function(test, consequent) {
 };
 ASTCompiler.prototype.assign = function(id, value) {
   return id + '=' + value + ';';
+};
+ASTCompiler.prototype.addEnsureSafeMemberName = function(expr) {
+  this.state.body.push('ensureSafeMemberName(' + expr + ');');
 };
 
 ASTCompiler.prototype.stringEscapeRegex = /[^ a-zA-Z0-9]/g;
