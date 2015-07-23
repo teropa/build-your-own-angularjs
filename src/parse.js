9b3622d251cc6d90064cc2f2c7ec50121f60306c
Tero Parviainen <tero@teropa.info>
2015-07-23 10:40:15 +0300

    Statements

--- src/parse.js
+++ src/parse.js
@@ -84,7 +84,7 @@ Lexer.prototype.lex = function(text) {
       this.readNumber();
     } else if (this.is('\'"')) {
       this.readString(this.ch);
-    } else if (this.is('[],{}:.()?')) {
+    } else if (this.is('[],{}:.()?;')) {
       this.tokens.push({
         text: this.ch
       });
@@ -254,7 +254,15 @@ AST.prototype.ast = function(text) {
   return this.program();
 };
 AST.prototype.program = function() {
-  return {type: AST.Program, body: this.assignment()};
+  var body = [];
+  while (true) {
+    if (this.tokens.length) {
+      body.push(this.assignment());
+    }
+    if (!this.expect(';')) {
+      return {type: AST.Program, body: body};
+    }
+  }
 };
 AST.prototype.assignment = function() {
   var left = this.ternary();
@@ -522,7 +530,10 @@ ASTCompiler.prototype.recurse = function(ast, context, create) {
   var intoId;
   switch (ast.type) {
   case AST.Program:
-    this.state.body.push('return ', this.recurse(ast.body), ';');
+    _.forEach(_.initial(ast.body), _.bind(function(stmt) {
+      this.state.body.push(this.recurse(stmt), ';');
+    }, this));
+    this.state.body.push('return ', this.recurse(_.last(ast.body)), ';');
     break;
   case AST.Literal:
     return this.escape(ast.value);
