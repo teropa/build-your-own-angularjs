9de82ce238e83d90594afd7cfdf58e42c20d10fa
Tero Parviainen <tero@teropa.info>
2015-07-23 13:18:22 +0300

    One-Time Expressions

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -760,6 +760,76 @@ describe("Scope", function() {
       expect(scope.$$watchers.length).toBe(0);
     });
 
+    it('accepts one-time watches', function() {
+      var theValue;
+
+      scope.aValue = 42;
+      scope.$watch('::aValue', function(newValue, oldValue, scope) {
+        theValue = newValue;
+      });
+      scope.$digest();
+
+      expect(theValue).toBe(42);
+    });
+
+    it('removes one-time watches after first invocation', function() {
+      scope.aValue = 42;
+      scope.$watch('::aValue', function() { });
+      scope.$digest();
+
+      expect(scope.$$watchers.length).toBe(0);
+    });
+
+    it('does not remove one-time-watches until value is defined', function() {
+      scope.$watch('::aValue', function() { });
+
+      scope.$digest();
+      expect(scope.$$watchers.length).toBe(1);
+
+      scope.aValue = 42;
+      scope.$digest();
+      expect(scope.$$watchers.length).toBe(0);
+    });
+
+    it('does not remove one-time-watches until value stays defined', function() {
+      scope.aValue = 42;
+
+      scope.$watch('::aValue', function() { });
+      var unwatchDeleter = scope.$watch('aValue', function() {
+        delete scope.aValue;
+      });
+
+      scope.$digest();
+      expect(scope.$$watchers.length).toBe(2);
+
+      scope.aValue = 42;
+      unwatchDeleter();
+      scope.$digest();
+      expect(scope.$$watchers.length).toBe(0);
+    });
+
+    it('does not remove one-time watches before all array items defined', function() {
+      scope.$watch('::[1, 2, aValue]', function() { }, true);
+
+      scope.$digest();
+      expect(scope.$$watchers.length).toBe(1);
+
+      scope.aValue = 3;
+      scope.$digest();
+      expect(scope.$$watchers.length).toBe(0);
+    });
+
+    it('does not remove one-time watches before all object vals defined', function() {
+      scope.$watch('::{a: 1, b: aValue}', function() { }, true);
+
+      scope.$digest();
+      expect(scope.$$watchers.length).toBe(1);
+
+      scope.aValue = 3;
+      scope.$digest();
+      expect(scope.$$watchers.length).toBe(0);
+    });
+
   });
 
   describe('$watchGroup', function() {
