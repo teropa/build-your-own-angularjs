58d1079da97f646f3ae2f07666fffbd3d177ef49
Tero Parviainen <tero@teropa.info>
2015-07-23 10:14:33 +0300

    Altering The Precedence Order with Parentheses

--- src/parse.js
+++ src/parse.js
@@ -368,7 +368,10 @@ AST.prototype.unary = function() {
 };
 AST.prototype.primary = function() {
   var primary;
-  if (this.expect('[')) {
+  if (this.expect('(')) {
+    primary = this.assignment();
+    this.consume(')');
+  } else if (this.expect('[')) {
     primary = this.arrayDeclaration();
   } else if (this.expect('{')) {
     primary = this.object();
