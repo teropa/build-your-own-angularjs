b90f72f3293f2bc662ebd449d9af6d01a03bcacf
Tero Parviainen <tero@teropa.info>
2015-07-30 10:02:19 +0300

    Additional Filter Arguments

--- src/parse.js
+++ src/parse.js
@@ -269,12 +269,16 @@ AST.prototype.program = function() {
 AST.prototype.filter = function() {
   var left = this.assignment();
   while (this.expect('|')) {
+    var args = [left];
     left = {
       type: AST.CallExpression,
       callee: this.identifier(),
-      arguments: [left],
+      arguments: args,
       filter: true
     };
+    while (this.expect(':')) {
+      args.push(this.assignment());
+    }
   }
   return left;
 };
