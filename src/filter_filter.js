3c296e3587fcfe170a7d593a282c8d7cf6860427
Tero Parviainen <tero@teropa.info>
2015-07-30 11:50:56 +0300

    Filtering With Other Primitives

--- src/filter_filter.js
+++ src/filter_filter.js
@@ -15,8 +15,14 @@ function deepCompare(actual, expected, comparator) {
 function createPredicateFn(expression) {
 
   function comparator(actual, expected) {
-    actual = actual.toLowerCase();
-    expected = expected.toLowerCase();
+    if (_.isUndefined(actual)) {
+      return false;
+    }
+    if (_.isNull(actual) || _.isNull(expected)) {
+      return actual === expected;
+    }
+    actual = ('' + actual).toLowerCase();
+    expected = ('' + expected).toLowerCase();
     return actual.indexOf(expected) !== -1;
   }
 
@@ -30,7 +36,10 @@ function filterFilter() {
     var predicateFn;
     if (_.isFunction(filterExpr)) {
       predicateFn = filterExpr;
-    } else if (_.isString(filterExpr)) {
+    } else if (_.isString(filterExpr) ||
+               _.isNumber(filterExpr) ||
+               _.isBoolean(filterExpr) ||
+               _.isNull(filterExpr)) {
       predicateFn = createPredicateFn(filterExpr);
     } else {
       return array;
