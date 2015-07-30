3e4c8c51688e7763c3cdc70cf6bf17aab9943126
Tero Parviainen <tero@teropa.info>
2015-07-30 11:34:12 +0300

    Filtering With Strings

--- src/filter_filter.js
+++ src/filter_filter.js
@@ -2,9 +2,40 @@
 
 var _ = require('lodash');
 
+function deepCompare(actual, expected, comparator) {
+  if (_.isObject(actual)) {
+    return _.some(actual, function(value, key) {
+      return deepCompare(value, expected, comparator);
+    });
+  } else {
+    return comparator(actual, expected);
+  }
+}
+
+function createPredicateFn(expression) {
+
+  function comparator(actual, expected) {
+    actual = actual.toLowerCase();
+    expected = expected.toLowerCase();
+    return actual.indexOf(expected) !== -1;
+  }
+
+  return function predicateFn(item) {
+    return deepCompare(item, expression, comparator);
+  };
+}
+
 function filterFilter() {
   return function(array, filterExpr) {
-    return _.filter(array, filterExpr);
+    var predicateFn;
+    if (_.isFunction(filterExpr)) {
+      predicateFn = filterExpr;
+    } else if (_.isString(filterExpr)) {
+      predicateFn = createPredicateFn(filterExpr);
+    } else {
+      return array;
+    }
+    return _.filter(array, predicateFn);
   };
 }
 
